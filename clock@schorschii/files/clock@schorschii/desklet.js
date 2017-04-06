26f3fd3c1cd2013eaaa9e4c9dcdf79c1c8eb6cf7
schorschii <schorschii@users.noreply.github.com>
2017-04-06 20:29:34 +0200

    clock@schorschii: Version 1.1 (#23)
    * clock@schorschii: Version 1.1
    
    - now using vector graphics instead of pixel graphics
    - improved settings description
    - fixed JS warning ("seconds_deg")
    
    * clock@schorschii: clean up
    
    removed no longer required file in version 1.1
    
    * clock@schorschii: clean up
    
    removed no longer required file in version 1.1
    
    * clock@schorschii: clean up
    
    removed no longer required file in version 1.1
    
    * clock@schorschii: clean up
    
    removed no longer required file in version 1.1
    
    * clock@schorschii: clean up
    
    removed no longer required file in version 1.1

--- clock@schorschii/files/clock@schorschii/desklet.js
+++ clock@schorschii/files/clock@schorschii/desklet.js
@@ -8,6 +8,10 @@ const Lang = imports.lang;
 const Settings = imports.ui.settings;
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
+const GdkPixbuf = imports.gi.GdkPixbuf;
+const Cogl = imports.gi.Cogl;
+
+const DESKLET_ROOT = imports.ui.deskletManager.deskletMeta["clock@schorschii"].path;
 
 
 function MyDesklet(metadata, desklet_id) {
@@ -18,6 +22,23 @@ function main(metadata, desklet_id) {
 	return new MyDesklet(metadata, desklet_id);
 }
 
+function getImageAtScale(imageFileName, width, height) {
+	let pixBuf = GdkPixbuf.Pixbuf.new_from_file_at_size(imageFileName, width, height);
+	let image = new Clutter.Image();
+	image.set_data(
+		pixBuf.get_pixels(),
+		pixBuf.get_has_alpha() ? Cogl.PixelFormat.RGBA_8888 : Cogl.PixelFormat.RGBA_888,
+		width, height,
+		pixBuf.get_rowstride()
+	);
+
+	let actor = new Clutter.Actor({width: width, height: height});
+	actor.set_content(image);
+
+	return actor;
+}
+
+
 MyDesklet.prototype = {
 	__proto__: Desklet.Desklet.prototype,
 
@@ -26,7 +47,7 @@ MyDesklet.prototype = {
 
 		// initialize settings
 		this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], desklet_id);
-		this.settings.bindProperty(Settings.BindingDirection.IN, "scale-size", "scale_size", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.IN, "desklet-size", "desklet_size", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "show-seconds-hand", "show_seconds_hand", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "smooth-seconds-hand", "smooth_seconds_hand", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "hide-decorations", "hide_decorations", this.on_setting_changed);
@@ -44,39 +65,13 @@ MyDesklet.prototype = {
 	},
 
 	setupUI: function() {
-		// set default sizes
-		this.default_size_clock_width = 130;
-		this.default_size_clock_height = 130;
-		this.calced_size_clock_width = 0;
-		this.calced_size_clock_height = 0;
-
-		// create elements
-		this.clock = new St.Bin({style_class: 'clock'});
-		this.clock_container = new St.Group({style_class: 'clock_container'}); // container for pointers with background image
-		this.second_hand = new St.Bin({style_class: 'hand second_hand'}); // pointers
-		this.minute_hand = new St.Bin({style_class: 'hand minute_hand'});
-		this.hour_hand = new St.Bin({style_class: 'hand hour_hand'});
-
-		// add actors
-		this.clock_container.add_actor(this.second_hand);
-		this.clock_container.add_actor(this.minute_hand);
-		this.clock_container.add_actor(this.hour_hand);
-		this.clock.add_actor(this.clock_container);
-
-		// set pivot points for pointers
-		this.second_hand.set_pivot_point(0.5,0.5);
-		this.minute_hand.set_pivot_point(0.5,0.5);
-		this.hour_hand.set_pivot_point(0.5,0.5);
-
-		// set root element
-		this.setContent(this.clock);
+		// load images and add actors
+		this.refreshSize();
 
 		// set initial size and decoration settings
-		this.refreshSize();
 		this.refreshDecoration();
 
 		// set initial values
-		this.is_first_refresh = true;
 		this.refresh();
 	},
 
@@ -99,7 +94,7 @@ MyDesklet.prototype = {
 		// calc pointer rotation angles
 		let hours_deg = 0;
 		let minutes_deg = 0;
-		let hours_deg = 0;
+		let seconds_deg = 0;
 		if (this.smooth_seconds_hand == true) {
 			hours_deg = ((hours+(minutes/60))*360/12);
 			minutes_deg = ((minutes+(seconds/60))*360/60);
@@ -115,49 +110,64 @@ MyDesklet.prototype = {
 		this.minute_hand.set_rotation_angle(Clutter.RotateAxis.Z_AXIS, minutes_deg);
 		this.hour_hand.set_rotation_angle(Clutter.RotateAxis.Z_AXIS, hours_deg);
 
-		// refresh again in 100 milliseconds
-		this.timeout = Mainloop.timeout_add(100, Lang.bind(this, this.refresh));
+		// refresh again in 100 milliseconds or 1 second
+		if (this.smooth_seconds_hand == true)
+			this.timeout = Mainloop.timeout_add(100, Lang.bind(this, this.refresh));
+		else
+			this.timeout = Mainloop.timeout_add(1000, Lang.bind(this, this.refresh));
 	},
 
-	refreshSize: function(recalc = true) {
-		// calc sizes
-		if (recalc == true) {
-			this.calced_size_clock_width = this.default_size_clock_width * this.scale_size;
-			this.calced_size_clock_height = this.default_size_clock_height * this.scale_size;
-		}
+	refreshSize: function() {
+		// create elements
+		this.clock = new St.Bin({style_class: 'clock'});
+		this.clock_container = new St.Group({style_class: 'clock_container'}); // container for pointers with background image
 
-		// set images
+		// defaults
+		let img_bg_final = DESKLET_ROOT + "/img/bg.svg";
+		let img_h_final = DESKLET_ROOT + "/img/h.svg";
+		let img_m_final = DESKLET_ROOT + "/img/m.svg";
+		let img_s_final = DESKLET_ROOT + "/img/s.svg";
+
+		// override defaults if images are set
+		if (this.desklet_size < 10)
+			this.desklet_size = 10;
 		if (this.img_bg != "")
-			this.img_bg_style = "background-image: url('" + this.img_bg.replace("file://", "") + "');";
-		else
-			this.img_bg_style = "";
+			img_bg_final = this.img_bg.replace("file://", "");
 		if (this.img_s != "")
-			this.img_s_style = "background-image: url('" + this.img_s.replace("file://", "") + "');";
-		else
-			this.img_s_style = "";
+			img_s_final = this.img_s.replace("file://", "");
 		if (this.img_m != "")
-			this.img_m_style = "background-image: url('" + this.img_m.replace("file://", "") + "');";
-		else
-			this.img_m_style = "";
+			img_m_final = this.img_m.replace("file://", "");
 		if (this.img_h != "")
-			this.img_h_style = "background-image: url('" + this.img_h.replace("file://", "") + "');";
-		else
-			this.img_h_style = "";
+			img_h_final = this.img_h.replace("file://", ""); 
 
 		// set sizes
-		this.size_style = "width: " + this.calced_size_clock_width.toString() + "px;" +
-		                  "height: " + this.calced_size_clock_height.toString() + "px;" +
-		                  "background-size: " + this.calced_size_clock_width.toString() + "px " + this.calced_size_clock_height.toString() + "px;";
+		this.size_style = "width: " + this.desklet_size.toString() + "px;" +
+		                  "height: " + this.desklet_size.toString() + "px;";
 		this.clock.style = this.size_style;
-		this.clock_container.style = this.size_style + this.img_bg_style;
-		this.minute_hand.style = this.size_style + this.img_m_style;
-		this.hour_hand.style = this.size_style + this.img_h_style;
+		this.clock_container.style = this.size_style;
 
-		if (this.show_seconds_hand == true) {
-			this.second_hand.style = this.size_style + this.img_s_style;
-		} else {
-			this.second_hand.style = "background-size: 0px 0px;" + this.img_s_style;
-		}
+		// load images in given size
+		this.clock_bg = getImageAtScale(img_bg_final, this.desklet_size, this.desklet_size); // background
+		this.second_hand = getImageAtScale(img_s_final, this.desklet_size, this.desklet_size); // pointers
+		this.minute_hand = getImageAtScale(img_m_final, this.desklet_size, this.desklet_size);
+		this.hour_hand = getImageAtScale(img_h_final, this.desklet_size, this.desklet_size);
+
+		// set pivot points for pointer images
+		this.second_hand.set_pivot_point(0.5,0.5);
+		this.minute_hand.set_pivot_point(0.5,0.5);
+		this.hour_hand.set_pivot_point(0.5,0.5);
+
+		// add actors
+		this.clock.remove_all_children();
+		this.clock_container.add_actor(this.clock_bg);
+		this.clock_container.add_actor(this.hour_hand);
+		this.clock_container.add_actor(this.minute_hand);
+		if (this.show_seconds_hand == true)
+			this.clock_container.add_actor(this.second_hand);
+
+		// set root element
+		this.clock.add_actor(this.clock_container);
+		this.setContent(this.clock);
 	},
 
 	refreshDecoration: function() {
@@ -176,12 +186,12 @@ MyDesklet.prototype = {
 		// update decoration settings
 		this.refreshDecoration();
 
+		// update size and graphics
+		this.refreshSize();
+
 		// settings changed; instant refresh
 		Mainloop.source_remove(this.timeout);
 		this.refresh();
-
-		// update size based on scale factor
-		this.refreshSize();
 	},
 
 	on_desklet_removed: function() {
