20ca1b25433e6c599abf3bce6d2124ab748c8b69
BrainAxe <tanzimrizwan@gmail.com>
2017-10-02 19:42:39 +0600

    Initial commit

---
+++ deskNote@BrainAxe/README.md
@@ -0,0 +1,21 @@
+# ![ScreenShot](icon.png) Desktop Note Cinnamon Desklet #
+
+v0.1 - October 2, 2017
+
+This desklet displays a note on the desktop. Right click it to configure at any time.
+
+## Configuration Options ##
+
+The following options are configurable via the Cinnamon Desklet GUI by right clicking "Desktop Note desklet" 
+and choosing Configure.
+
+- New Note
+- Font size
+- Font color
+
+For newline use % (symbol).
+
+**Important**: Cinnamon may need to be restarted and/or the desklet may need to be removed and re-added 
+for this change to take effect. 
+
+![ScreenShot](screenshot.png) 
---
+++ deskNote@BrainAxe/files/deskNote@BrainAxe/desklet.js
@@ -0,0 +1,79 @@
+const Cinnamon = imports.gi.Cinnamon;
+const Desklet = imports.ui.desklet;
+const Gettext = imports.gettext;
+const GLib = imports.gi.GLib;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Settings = imports.ui.settings;
+const St = imports.gi.St;
+const Util = imports.misc.util;
+const uuid = "inspireNote@BrainAxe";
+
+
+function deskNote(metadata, desklet_id) {
+    this._init(metadata, desklet_id);
+}
+
+deskNote.prototype = {
+    __proto__: Desklet.Desklet.prototype,
+
+    _init: function(metadata, desklet_id) {
+        Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
+
+        this._note = new St.Label({style_class: "note-container"});
+        this.setContent(this._note);
+        this.setHeader(_("Inspire Note"));
+
+        this.maxSize = 7000; // Cinnamon can crash if this int is too high
+
+        this.setupUI()
+
+        try {
+           this.settings = new Settings.DeskletSettings(
+               this, this.metadata["uuid"], this.instance_id);
+
+           this.settings.bindProperty(Settings.BindingDirection.IN, "note-entry", "noteEntry", this.on_setting_changed, null);
+           this.settings.bindProperty(Settings.BindingDirection.IN, "font-size", "fontSize", this.on_font_setting_changed, null);
+           this.settings.bindProperty(Settings.BindingDirection.IN, "text-color", "fontColor", this.on_font_setting_changed, null);
+
+         }
+       catch (e) {
+           global.logError(e);
+       }
+
+       this.on_setting_changed();
+       this.on_font_setting_changed();
+
+    },
+
+    on_setting_changed: function() {
+      var new_text = this.noteEntry;
+      var new_text = new_text.replace("%", "\n");
+      this.text.set_text(new_text);
+    },
+
+    on_font_setting_changed: function() {
+        this._note.style="font-size: " + this.fontSize + "pt;\n" +
+                          "color: " + this.fontColor + ";\n" ;
+    },
+
+
+
+    setupUI: function() {
+        // main container for the desklet
+        this.window = new St.Bin();
+        this.text = new St.Label();
+
+        this.text.set_text("");
+        this._note = new St.BoxLayout({vertical:true, style_class: 'note-container'})
+
+        this._note.add(this.text);
+
+        this.window.add_actor(this._note)
+        this.setContent(this.window);
+    }
+}
+
+function main(metadata, desklet_id) {
+    return new deskNote(metadata, desklet_id);
+}
