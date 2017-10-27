c4eed483b21e8205fefd4f301d61671dd980005d
Stephen Collins\n <scrivera64@gmail.com>
2017-10-27 16:51:50 -0600

    devtools: fix .xsession-errors not updating properly

--- devTools@scollins/files/devTools@scollins/errorLog.js
+++ devTools@scollins/files/devTools@scollins/errorLog.js
@@ -1,11 +1,13 @@
 const CheckBox = imports.ui.checkBox;
 const LookingGlass = imports.ui.lookingGlass;
 const Main = imports.ui.main;
+const Cinnamon = imports.gi.Cinnamon;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Pango = imports.gi.Pango;
 const St = imports.gi.St;
 const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 
 const uuid = "devTools@scollins";
 const DeskletDir = imports.ui.deskletManager.desklets[uuid];
@@ -31,25 +33,25 @@ function XSessionLogInterface(settings) {
 
 XSessionLogInterface.prototype = {
     __proto__: TabPanel.TabPanelBase.prototype,
-    
+
     name: _("X-Session Log"),
-    
+
     _init: function(settings) {
-        
+
         TabPanel.TabPanelBase.prototype._init.call(this);
-        
+
         this.settings = settings;
         this.start = 0;
-        
+
         //content text
         this.text = new Text.Label();
         this.panel.add(this.text.actor, { expand: true });
         this.contentText = this.text.label;
         this.scrollBox = this.text.scroll;
-        
+
         let bottomBox = new St.BoxLayout({ style_class: "devtools-log-bottomBox" });
         this.panel.add_actor(bottomBox);
-        
+
         this.hideOld = new CheckBox.CheckBox(_("Hide old errors"), { style_class: "devtools-checkBox" });
         bottomBox.add_actor(this.hideOld.actor);
         this.hideOld.actor.checked = this.settings.getValue("xsessionHideOld");
@@ -57,9 +59,9 @@ XSessionLogInterface.prototype = {
             this.settings.setValue("xsessionHideOld", this.hideOld.actor.checked);
             this.getText();
         }));
-        
+
         bottomBox.add(new St.BoxLayout(), { expand: true });
-        
+
         //clear button
         let clearButton = new St.Button({ style_class: "devtools-contentButton" });
         bottomBox.add_actor(clearButton);
@@ -67,44 +69,43 @@ XSessionLogInterface.prototype = {
         clearButton.add_actor(clearBox);
         clearBox.add_actor(new St.Label({ text: _("Clear") }));
         clearButton.connect("clicked", Lang.bind(this, this.clear));
-        
-        this.file = Gio.file_new_for_path(GLib.get_home_dir() + "/.xsession-errors")
-        let monitor = this.file.monitor_file(Gio.FileMonitorFlags.NONE, null);
-        monitor.connect("changed", Lang.bind(this, this.getText));
+
+        this.file = Gio.file_new_for_path(GLib.get_home_dir() + "/.xsession-errors");
     },
-    
+
     getText: function() {
         //if the tab is not shown don't waste resources on refreshing content
-        if ( !this.selected ) return;
-        
-        this.file.load_contents_async(null, Lang.bind(this, function(file, result) {
-            try {
-                let text = "";
-                let lines = String(file.load_contents_finish(result)[1]).split("\n");
-                this.end = lines.length - 1;
-                for ( let i = this.start; i < lines.length; i++ ) {
-                    let line = lines[i];
-                    if ( this.hideOld.actor.checked && line.search("About to start Cinnamon") > -1 ) {
-                        text = ""
-                    }
-                    text += line + "\n";
+        if ( !this.selected ) return false;
+
+        try {
+            let lines = Cinnamon.get_file_contents_utf8_sync(this.file.get_path()).split('\n');
+            let text = "";
+            this.end = lines.length - 1;
+            for ( let i = this.start; i < lines.length; i++ ) {
+                let line = lines[i];
+                if ( this.hideOld.actor.checked && line.search("About to start Cinnamon") > -1 ) {
+                    text = ""
                 }
-                
-                if ( this.contentText.text != text ) {
-                    this.contentText.text = text;
-                    let adjustment = this.scrollBox.get_vscroll_bar().get_adjustment();
-                    adjustment.value = this.contentText.height - adjustment.page_size;
-                }
-            } catch(e) {
-                global.logError(e);
+                text += line + "\n";
             }
-        }));
+
+            if ( this.contentText.text != text ) {
+                this.contentText.text = text;
+                let adjustment = this.scrollBox.get_vscroll_bar().get_adjustment();
+                adjustment.value = this.contentText.height - adjustment.page_size;
+            }
+        } catch(e) {
+            global.logError(e);
+        }
+
+        return true;
     },
-    
+
     onSelected: function() {
         this.getText();
+        Mainloop.timeout_add(500, Lang.bind(this, this.getText));
     },
-    
+
     clear: function() {
         this.start = this.end;
         this.getText();
@@ -118,24 +119,24 @@ function CinnamonLogInterface(settings) {
 
 CinnamonLogInterface.prototype = {
     __proto__: TabPanel.TabPanelBase.prototype,
-    
+
     name: _("Cinnamon Log"),
-    
+
     _init: function(settings) {
-        
+
         TabPanel.TabPanelBase.prototype._init.call(this);
-        
+
         this.settings = settings;
-        
+
         //content text
         this.text = new Text.Label();
         this.panel.add(this.text.actor, { expand: true });
         this.contentText = this.text.label;
         this.scrollBox = this.text.scroll;
-        
+
         let bottomBox = new St.BoxLayout({ style_class: "devtools-log-bottomBox" });
         this.panel.add_actor(bottomBox);
-        
+
         this.showTimestamp = new CheckBox.CheckBox(_("Show Timestamp"), { style_class: "devtools-checkBox" });
         bottomBox.add_actor(this.showTimestamp.actor);
         this.showTimestamp.actor.checked = this.settings.getValue("clTimestamp");
@@ -143,7 +144,7 @@ CinnamonLogInterface.prototype = {
             this.settings.setValue("clTimestamp", this.showTimestamp.actor.checked);
             this.getText();
         }));
-        
+
         this.infos = new CheckBox.CheckBox(_("Infos"), { style_class: "devtools-checkBox" });
         bottomBox.add_actor(this.infos.actor);
         this.infos.actor.checked = this.settings.getValue("clShowInfos");
@@ -151,7 +152,7 @@ CinnamonLogInterface.prototype = {
             this.settings.setValue("clShowInfos", this.infos.actor.checked);
             this.getText();
         }));
-        
+
         this.warnings = new CheckBox.CheckBox(_("Warnings"), { style_class: "devtools-checkBox" });
         bottomBox.add_actor(this.warnings.actor);
         this.warnings.actor.checked = this.settings.getValue("clShowWarnings");
@@ -159,7 +160,7 @@ CinnamonLogInterface.prototype = {
             this.settings.setValue("clShowWarnings", this.warnings.actor.checked);
             this.getText();
         }));
-        
+
         this.errors = new CheckBox.CheckBox(_("Errors"), { style_class: "devtools-checkBox" });
         bottomBox.add_actor(this.errors.actor);
         this.errors.actor.checked = this.settings.getValue("clShowErrors");
@@ -167,7 +168,7 @@ CinnamonLogInterface.prototype = {
             this.settings.setValue("clShowErrors", this.errors.actor.checked);
             this.getText();
         }));
-        
+
         this.traces = new CheckBox.CheckBox(_("Traces"), { style_class: "devtools-checkBox" });
         bottomBox.add_actor(this.traces.actor);
         this.traces.actor.checked = this.settings.getValue("clShowTraces");
@@ -175,9 +176,9 @@ CinnamonLogInterface.prototype = {
             this.settings.setValue("clShowTraces", this.traces.actor.checked);
             this.getText();
         }));
-        
+
         bottomBox.add(new St.BoxLayout(), { expand: true });
-        
+
         let copyButton = new St.Button({ style_class: "devtools-contentButton" });
         let copyBox = new St.BoxLayout();
         copyButton.add_actor(copyBox);
@@ -185,10 +186,10 @@ CinnamonLogInterface.prototype = {
         copyBox.add_actor(new St.Label({ text: _("Copy") }));
         bottomBox.add_actor(copyButton);
         copyButton.connect("clicked", Lang.bind(this, this.copy));
-        
+
         this.connectToLgDBus();
     },
-    
+
     connectToLgDBus: function() {
         let proxy = Gio.DBusProxy.makeProxyWrapper(LookingGlass.lgIFace);
         new proxy(Gio.DBus.session, "org.Cinnamon.LookingGlass", "/org/Cinnamon/LookingGlass", Lang.bind(this, function(proxy, error) {
@@ -196,13 +197,13 @@ CinnamonLogInterface.prototype = {
             this._proxy.connectSignal("LogUpdate", Lang.bind(this, this.getText));
         }));
     },
-    
+
     getText: function() {
         //if the tab is not shown don't waste resources on refreshing content
         if ( !this.selected ) return;
-        
+
         let stack = Main._errorLogStack;
-        
+
         let text = "";
         for ( let i = 0; i < stack.length; i++) {
             let logItem = stack[i];
@@ -224,16 +225,16 @@ CinnamonLogInterface.prototype = {
             if ( this.showTimestamp.actor.checked ) text += this._formatTime(new Date(parseInt(logItem.timestamp)));
             text += logItem.message + "\n";
         }
-        
+
         this.contentText.text = text;
         let adjustment = this.scrollBox.get_vscroll_bar().get_adjustment();
         adjustment.value = this.contentText.height - this.text.actor.height;
     },
-    
+
     onSelected: function() {
         this.getText();
     },
-    
+
     copy: function() {
         St.Clipboard.get_default().set_text(this.contentText.text);
     }
