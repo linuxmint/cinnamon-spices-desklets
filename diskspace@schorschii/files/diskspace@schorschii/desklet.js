ca524ab7f7a51692a618a70c8cdddbfdcb007737
Georg <schorschii@users.noreply.github.com>
2018-02-16 15:58:10 +0100

    diskspace@schorschii: Version 1.1 (#261)
    - random circle color
    - option to display filesystem name, free or used space as text
    - updated german translation

--- diskspace@schorschii/files/diskspace@schorschii/desklet.js
+++ diskspace@schorschii/files/diskspace@schorschii/desklet.js
@@ -42,8 +42,20 @@ MyDesklet.prototype = {
 		this.settings.bindProperty(Settings.BindingDirection.IN, "scale-size", "scale_size", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "text-color", "text_color", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "circle-color", "circle_color", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.IN, "use-own-circle-color", "use_own_circle_color", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "filesystem", "filesystem", this.on_setting_changed);
 		this.settings.bindProperty(Settings.BindingDirection.IN, "type", "type", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.IN, "text-view", "text_view", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.INOUT, "random-circle-color-generated", "random_circle_color_generated", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.INOUT, "random-circle-color-r", "random_circle_color_r", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.INOUT, "random-circle-color-g", "random_circle_color_g", this.on_setting_changed);
+		this.settings.bindProperty(Settings.BindingDirection.INOUT, "random-circle-color-b", "trandom_circle_color_b", this.on_setting_changed);
+		if(!this.random_circle_color_generated) {
+			this.random_circle_color_r = Math.random();
+			this.random_circle_color_g = Math.random();
+			this.random_circle_color_b = Math.random();
+			this.random_circle_color_generated = true;
+		}
 
 		// initialize desklet gui
 		this.setupUI();
@@ -51,12 +63,8 @@ MyDesklet.prototype = {
 
 	setupUI: function() {
 		// defaults and initial values
-		this.default_notepad_label_top = 23;
-		this.default_notepad_label_left = 21;
-		this.default_notepad_label_right = 15;
-		this.default_notepad_label_bottom = 10;
-		this.default_size_font = 20;
-		this.default_size_font_sub = 8;
+		this.default_size_font = 28;
+		this.default_size_font_sub = 13;
 
 		// load images and set initial sizes
 		this.refreshSize();
@@ -80,25 +88,27 @@ MyDesklet.prototype = {
 	},
 
 	refreshSize: function() {
-
 			// calc new sizes based on scale factor
-			let absolute_size = 100 * this.scale_size;
-			let font_size = this.default_size_font * this.scale_size;
-			let font_size_sub = this.default_size_font_sub * this.scale_size;
-			this.desklet_width = this.size_width * this.scale_size;
-			this.desklet_height = this.size_height * this.scale_size;
-			this.label_top = this.default_notepad_label_top * this.scale_size;
-			this.label_left = this.default_notepad_label_left * this.scale_size;
-			this.label_right = this.default_notepad_label_right * this.scale_size;
-			this.label_bottom = this.default_notepad_label_bottom * this.scale_size;
+			let absolute_size = 150 * this.scale_size;
+			let font_size = Math.round(this.default_size_font * this.scale_size);
+			let font_size_sub = Math.round(this.default_size_font_sub * this.scale_size);
 
 			// calc colors
-			let circle_colors = this.circle_color.match(/\((.*?)\)/)[1].split(","); // get contents inside brackets: "rgb(...)"
-			var circle_r = parseInt(circle_colors[0])/255;
-			var circle_g = parseInt(circle_colors[1])/255;
-			var circle_b = parseInt(circle_colors[2])/255;
+			var circle_r = 1;
+			var circle_g = 1;
+			var circle_b = 1;
 			var circle_a = 1;
-			if(circle_colors.length >= 4) circle_a = parseFloat(circle_colors[3]);
+			if(this.use_own_circle_color) {
+				let circle_colors = this.circle_color.match(/\((.*?)\)/)[1].split(","); // get contents inside brackets: "rgb(...)"
+				circle_r = parseInt(circle_colors[0])/255;
+				circle_g = parseInt(circle_colors[1])/255;
+				circle_b = parseInt(circle_colors[2])/255;
+				if(circle_colors.length >= 4) circle_a = parseFloat(circle_colors[3]);
+			} else {
+				circle_r = this.random_circle_color_r;
+				circle_g = this.random_circle_color_g;
+				circle_b = this.random_circle_color_b;
+			}
 
 			// get filesystem
 			var type = this.type;
@@ -123,20 +133,7 @@ MyDesklet.prototype = {
 				//cr.setLineCap(Cairo.LineCap.ROUND);
 				cr.translate(0.5, 0.5);
 
-				if(type == "filesystem") {
-					let subprocess = new Gio.Subprocess({
-						argv: ['/bin/df', fs],
-						flags: Gio.SubprocessFlags.STDOUT_PIPE,
-					});
-					subprocess.init(null);
-					let [, out] = subprocess.communicate_utf8(null, null); // get full output from stdout
-					let fsline = out.split(/\r?\n/)[1]; // get second line with fs information
-					let fsvalues = fsline.split(/\s+/); // separate space-separated values from line
-					avail = parseInt(fsvalues[3]);
-					use = parseInt(fsvalues[2]);
-					size = parseInt(fsvalues[1]);
-					percent_string = fsvalues[4];
-				} else {
+				if(type == "ram") {
 					let subprocess = new Gio.Subprocess({
 						argv: ['/usr/bin/free'],
 						flags: Gio.SubprocessFlags.STDOUT_PIPE,
@@ -149,6 +146,19 @@ MyDesklet.prototype = {
 					use = parseInt(fsvalues[2]);
 					size = parseInt(fsvalues[1]);
 					percent_string = Math.round(use * 100 / size) + "%";
+				} else {
+					let subprocess = new Gio.Subprocess({
+						argv: ['/bin/df', fs],
+						flags: Gio.SubprocessFlags.STDOUT_PIPE,
+					});
+					subprocess.init(null);
+					let [, out] = subprocess.communicate_utf8(null, null); // get full output from stdout
+					let fsline = out.split(/\r?\n/)[1]; // get second line with fs information
+					let fsvalues = fsline.split(/\s+/); // separate space-separated values from line
+					avail = parseInt(fsvalues[3]);
+					use = parseInt(fsvalues[2]);
+					size = parseInt(fsvalues[1]);
+					percent_string = fsvalues[4];
 				}
 
 				let offset = Math.PI*0.5;
@@ -165,7 +175,6 @@ MyDesklet.prototype = {
 				cr.arc(0, 0, 0.4, start, end);
 				cr.stroke();
 
-				//cr.setSourceRGBA(71/255, 172/255, 138/255, 0.5);
 				cr.setSourceRGBA(0, 0, 0, 0.1446);
 				cr.setLineWidth(0.05);
 				cr.arc(0, 0, 0.325, start, end);
@@ -181,8 +190,25 @@ MyDesklet.prototype = {
 
 			this.container = new St.Group(); // container for labels
 
-			let sub_string = this.niceSize(use);
-			let sub_string2 = this.niceSize(size);
+			let sub_string = "";
+			let sub_string2 = "";
+			if(this.text_view == "name-size") {
+				if(type == "ram") {
+					sub_string = this.shortText(_("RAM"));
+				} else if(fs == "/") {
+					sub_string = this.shortText(_("Filesystem"));
+				} else {
+					pathparts = fs.split("/");
+					sub_string = this.shortText(pathparts[pathparts.length-1]);
+				}
+				sub_string2 = this.niceSize(size);
+			} else if(this.text_view == "used-size") {
+				sub_string = this.niceSize(use);
+				sub_string2 = this.niceSize(size);
+			} else if(this.text_view == "free-size") {
+				sub_string = this.niceSize(avail);
+				sub_string2 = this.niceSize(size);
+			}
 
 			// create labels
 			let textpercent_x = (absolute_size / 2) - ((font_size*percent_string.length/1.6) / 2);
@@ -190,28 +216,22 @@ MyDesklet.prototype = {
 			let textpercent = new St.Label({style_class:"textpercent"});
 			textpercent.set_position(textpercent_x, textpercent_y);
 			textpercent.set_text(percent_string);
-			textpercent.style = "width: auto;"
-			                    + "height: auto;"
-			                    + "font-size: " + font_size + "px;"
-			                    + "color:" + this.text_color + ";";
+			textpercent.style = "font-size: " + font_size + "px;"
+			                  + "color: " + this.text_color + ";";
 			let textsub_x = (absolute_size / 2) - ((font_size_sub*sub_string.length/1.6) / 2);
 			let textsub_y = textpercent_y + (font_size*1.4);
 			let textsub = new St.Label({style_class:"textsub"});
 			textsub.set_position(textsub_x, textsub_y);
 			textsub.set_text(sub_string);
-			textsub.style = "width: auto;"
-			                    + "height: auto;"
-			                    + "font-size: " + font_size_sub + "px;"
-			                    + "color:" + this.text_color + ";";
+			textsub.style = "font-size: " + font_size_sub + "px;"
+			              + "color: " + this.text_color + ";";
 			let textsub2_x = (absolute_size / 2) - ((font_size_sub*sub_string2.length/1.6) / 2);
 			let textsub2_y = textsub_y + (font_size_sub*1.25);
 			let textsub2 = new St.Label({style_class:"textsub2"});
 			textsub2.set_position(textsub2_x, textsub2_y);
 			textsub2.set_text(sub_string2);
-			textsub2.style = "width: auto;"
-			                    + "height: auto;"
-			                    + "font-size: " + font_size_sub + "px;"
-			                    + "color:" + this.text_color + ";";
+			textsub2.style = "font-size: " + font_size_sub + "px;"
+			               + "color: " + this.text_color + ";";
 
 			// add actor
 			this.canvas.remove_all_children();
@@ -229,20 +249,33 @@ MyDesklet.prototype = {
 		if(use_binary) {
 			if(value < 1024) return value + "K";
 			else if(value < 1024*1024) return Math.round(value / 1024 * 10) / 10 + "M";
-			else return Math.round(value / 1024 / 1024 * 10) / 10 + "G";
+			else if(value < 1024*1024*1024) return Math.round(value / 1024 / 1024 * 10) / 10 + "G";
+			else return Math.round(value / 1024 / 1024 / 1024 * 10) / 10 + "T";
 		} else {
 			if(value < 1000) return value + "K";
 			else if(value < 1000*1000) return Math.round(value / 1000 * 10) / 10 + "M";
-			else return Math.round(value / 1000 / 1000 * 10) / 10 + "G";
+			else if(value < 1000*1000*1000) return Math.round(value / 1000 / 1000 * 10) / 10 + "G";
+			else return Math.round(value / 1000 / 1000 / 1000 * 10) / 10 + "T";
 		}
+	},
+
+	shortText: function(value) {
+		let max = 10;
+		return (value.length > max) ? value.substr(0, max-1) + '…' : value;
 	},
 
 	refreshDecoration: function() {
 		// desklet label (header)
 		if (this.use_custom_label == true)
 			this.setHeader(this.custom_label)
-		else
+		else if (this.type == "ram")
+			this.setHeader(_("RAM"));
+		else if (this.filesystem == null || this.filesystem == "" || this.filesystem == "/")
 			this.setHeader(_("Disk Space"));
+		else {
+			let pathparts = this.filesystem.replace("file://", "").trim().split("/");
+			this.setHeader(pathparts[pathparts.length-1]);
+		}
 
 		// prevent decorations?
 		this.metadata["prevent-decorations"] = this.hide_decorations;
