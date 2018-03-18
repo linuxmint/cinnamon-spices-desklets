511f5695dc03569d84354af72e5e92b23c48dd98
kanchudeep <30999796+kanchudeep@users.noreply.github.com>
2018-03-18 10:37:01 +0530

    Calendar Desklet: Contributing (#278)
    * Add files via upload
    
    * Add files via upload
    
    * Delete screenshot.png
    
    * Delete info.json
    
    * Delete icon.png
    
    * Delete README.md
    
    * Delete nepalipatro@deeppradhan.pot
    
    * Delete desklet.js
    
    * Delete icon.png
    
    * Delete metadata.json
    
    * Delete nepalipatro.js
    
    * Delete settings-schema.json
    
    * Create .editorconfig
    
    * Update .editorconfig
    
    * Update .editorconfig
    
    * Add files via upload
    
    * Create .editorconfig
    
    * Changed const to var for export
    
    * Improved Calendar import
    
    * Changed const to var
    
    * Update desklet.js
    
    * Update desklet.js
    
    * Update desklet.js
    
    * Remove nepalipatro@deeppradhan

---
+++ calendar@deeppradhan/files/calendar@deeppradhan/calendar.js
@@ -0,0 +1,25 @@
+// Names of the Month
+var MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"];
+
+// Names of the Weekdays
+var WEEKDAY_NAMES = ["Sunday", "Monday", "Tuesday",
+		"Wednesday", "Thursday", "Friday", "Saturday"];
+
+// Method to return a Date (first day) after adding specified months to specified Date
+function dateMonthAdd(date, add) {
+	return new Date(date.getFullYear(), date.getMonth() + add, 1);
+}
+
+// Method to return the number of days in a given month of a given year
+function daysInMonth(month, fullYear) {
+	return new Date(fullYear, month + 1, 0).getDate();
+}
+
+// Method to return a string with single digit number zero padded
+function zeroPad(number) {
+	number = Number(number);
+	if (typeof number !== "number")
+		return;
+	return (number >= 0 && number <= 9 ? "0" : "") + number;
+}
