77febd36a728391cd719816701fea55399e3d091
Bojan Petkovski <petk.bojan@gmail.com>
2018-12-02 20:03:03 +0100

    Change to double quotes

--- cryptocoins@pbojan/files/cryptocoins@pbojan/desklet.js
+++ cryptocoins@pbojan/files/cryptocoins@pbojan/desklet.js
@@ -53,11 +53,11 @@ HelloDesklet.prototype = {
       this.cfgCurrency = this.cfgCurrency.toUpperCase() || "USD";
       this.cfgRefreshInterval = this.cfgRefreshInterval || 30;
 
-      this._menu.addAction('Help', Lang.bind(this, function() {
-        Util.spawnCommandLine('xdg-open ' + HELP_URL);
+      this._menu.addAction("Help", Lang.bind(this, function() {
+        Util.spawnCommandLine("xdg-open " + HELP_URL);
       }));
       this._menu.addAction('Donate', Lang.bind(this, function() {
-        Util.spawnCommandLine('xdg-open ' + DONATE_URL);
+        Util.spawnCommandLine("xdg-open " + DONATE_URL);
       }));
 
       this.fetchData(true);
@@ -92,13 +92,13 @@ HelloDesklet.prototype = {
   showNoApiKey: function() {
     var container = new St.BoxLayout({
       vertical: true,
-      style_class: 'container'
+      style_class: "container"
     });
 
     var label = new St.Label({
-      style_class: 'apikey'
+      style_class: "apikey"
     });
-    label.set_text('Please set your api key in configuration...');
+    label.set_text("Please set your api key in configuration...");
     container.add(label);
 
     this.setContent(container);
@@ -107,13 +107,13 @@ HelloDesklet.prototype = {
   showLoading: function() {
     var container = new St.BoxLayout({
       vertical: true,
-      style_class: 'container'
+      style_class: "container"
     });
 
     var label = new St.Label({
-      style_class: 'loading'
+      style_class: "loading"
     });
-    label.set_text('Loading coin data...');
+    label.set_text("Loading coin data...");
     container.add(label);
 
     this.setContent(container);
@@ -128,26 +128,26 @@ HelloDesklet.prototype = {
     this.showLoading();
 
     var message = Soup.Message.new(
-        'GET',
-        API_URL + '?id=' + parseInt(this.cfgCoin) + '&convert=' + this.cfgCurrency
+        "GET",
+        API_URL + "?id=" + parseInt(this.cfgCoin) + "&convert=" + this.cfgCurrency
     );
-    message.request_headers.append('X-CMC_PRO_API_KEY', this.cfgApiKey);
+    message.request_headers.append("X-CMC_PRO_API_KEY", this.cfgApiKey);
 
     httpSession.queue_message(message,
       Lang.bind(this, function(session, response) {
         if (response.status_code !== Soup.KnownStatusCode.OK) {
-          global.log('Error during download: response code ' +
-              response.status_code + ': ' + response.reason_phrase + ' - ' +
+          global.log("Error during download: response code " +
+              response.status_code + ": " + response.reason_phrase + " - " +
               response.response_body.data);
           return;
         }
 
         var result = JSON.parse(message.response_body.data).data[this.cfgCoin];
         if (initUI === true) {
-          global.log('Init UI, create all objects for coin: ' + result['name']);
+          global.log("Init UI, create all objects for coin: " + result["name"]);
           this.setupUI(result);
         } else {
-          global.log('Update objects for coin: ' + result['name']);
+          global.log("Update objects for coin: " + result["name"]);
           this.updateUI(result);
         }
       })
@@ -163,14 +163,14 @@ HelloDesklet.prototype = {
   },
 
   updateUI: function(data) {
-    var quote = data['quote'][this.cfgCurrency];
+    var quote = data["quote"][this.cfgCurrency];
 
-    this.priceLabel.set_text(this.getFormattedPrice(quote['price']));
-    this.setChangeData(this.change1H, quote['percent_change_1h']);
-    this.setChangeData(this.change1D, quote['percent_change_24h']);
-    this.setChangeData(this.change7D, quote['percent_change_7d']);
+    this.priceLabel.set_text(this.getFormattedPrice(quote["price"]));
+    this.setChangeData(this.change1H, quote["percent_change_1h"]);
+    this.setChangeData(this.change1D, quote["percent_change_24h"]);
+    this.setChangeData(this.change7D, quote["percent_change_7d"]);
 
-    var date = new Date(data['last_updated'] * 1000);
+    var date = new Date(data["last_updated"] * 1000);
     this.lastUpdatedLabel.set_text(date.toLocaleString());
   },
 
@@ -178,21 +178,21 @@ HelloDesklet.prototype = {
     this.container = new St.BoxLayout({
       vertical: true,
       width: WIDTH,
-      style_class: 'container'
+      style_class: "container"
     });
 
-    var quote = data['quote'][this.cfgCurrency];
+    var quote = data["quote"][this.cfgCurrency];
 
     this.change1H = new St.Label();
     this.change1D = new St.Label();
     this.change7D = new St.Label();
 
     this.container.add(this.addHeaderAndTitle(data));
-    this.container.add(this.addPrice(quote['price']));
-    this.container.add(this.addChange('Change 1H:', this.change1H, quote['percent_change_1h']));
-    this.container.add(this.addChange('Change 1D:', this.change1D, quote['percent_change_24h']));
-    this.container.add(this.addChange('Change 7D:', this.change7D, quote['percent_change_7d']));
-    this.container.add(this.addLastUpdated(data['last_updated']));
+    this.container.add(this.addPrice(quote["price"]));
+    this.container.add(this.addChange("Change 1H:", this.change1H, quote["percent_change_1h"]));
+    this.container.add(this.addChange("Change 1D:", this.change1D, quote["percent_change_24h"]));
+    this.container.add(this.addChange("Change 7D:", this.change7D, quote["percent_change_7d"]));
+    this.container.add(this.addLastUpdated(data["last_updated"]));
 
     this.setContent(this.container);
   },
@@ -200,38 +200,38 @@ HelloDesklet.prototype = {
   addHeaderAndTitle: function(data) {
     var row = new St.BoxLayout({
       vertical: false,
-      style_class: 'row'
+      style_class: "row"
     });
     var left = new St.BoxLayout({
       vertical: true,
       width: WIDTH_ICON,
-      style_class: 'containerLeft'
+      style_class: "containerLeft"
     });
-    var file = Gio.file_new_for_path(DESKLET_ROOT + '/images/icons/' + data['symbol'].toLowerCase() + '.png');
+    var file = Gio.file_new_for_path(DESKLET_ROOT + "/images/icons/" + data["symbol"].toLowerCase() + ".png");
     var gicon = new Gio.FileIcon({file: file});
     var image = new St.Icon({
       gicon: gicon,
       icon_size: WIDTH_ICON,
       icon_type: St.IconType.SYMBOLIC,
-      style_class: 'icon'
+      style_class: "icon"
     });
     left.add(image);
 
     var right = new St.BoxLayout({
       vertical: true,
       width: WIDTH - PADDING - WIDTH_ICON,
-      style_class: 'containerRight'
+      style_class: "containerRight"
     });
     var label = new St.Label({
-      style_class: 'header'
+      style_class: "header"
     });
-    label.set_text(data['name']);
+    label.set_text(data["name"]);
     right.add(label);
 
     label = new St.Label({
-      style_class: 'headerID'
+      style_class: "headerID"
     });
-    label.set_text('(' + data['symbol'] + ') #' + data['cmc_rank']);
+    label.set_text("(" + data["symbol"] + ") #" + data["cmc_rank"]);
     right.add(label);
 
     row.add(left);
@@ -244,12 +244,12 @@ HelloDesklet.prototype = {
     var row = new St.BoxLayout({
       vertical: false,
       width: WIDTH - PADDING,
-      style_class: 'row'
+      style_class: "row"
     });
     var center = new St.BoxLayout({
       vertical: true,
       width: WIDTH - PADDING,
-      style_class: 'containerPrice'
+      style_class: "containerPrice"
     });
 
     this.priceLabel = new St.Label();
@@ -265,13 +265,13 @@ HelloDesklet.prototype = {
     var row = new St.BoxLayout({
       vertical: false,
       width: WIDTH - PADDING,
-      style_class: 'row containerCenter'
+      style_class: "row containerCenter"
     });
 
     var left = new St.BoxLayout({
       vertical: true,
       width: 110,
-      style_class: 'left'
+      style_class: "left"
     });
     var right = new St.BoxLayout({
       vertical: true,
@@ -295,29 +295,29 @@ HelloDesklet.prototype = {
   setChangeData: function(label, num) {
     num = parseFloat(num);
 
-    var cls = 'green';
+    var cls = "green";
     if (num < 0) {
-      cls = 'red';
+      cls = "red";
     }
 
     label.style_class = cls;
     label.set_text(num.toLocaleString(undefined, {
-      style: 'decimal',
+      style: "decimal",
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
-    }) + '%');
+    }) + "%");
   },
 
   addLastUpdated: function(date) {
     var row = new St.BoxLayout({
       vertical: false,
       width: WIDTH - PADDING,
-      style_class: 'row'
+      style_class: "row"
     });
     var right = new St.BoxLayout({
       vertical: true,
       width: WIDTH - PADDING,
-      style_class: 'lastUpdated'
+      style_class: "lastUpdated"
     });
 
     date = new Date(date);
@@ -332,13 +332,13 @@ HelloDesklet.prototype = {
 
   getFormattedPrice: function(price) {
     var options = {
-      style: 'currency',
+      style: "currency",
       currency: this.cfgCurrency.toLowerCase()
     };
 
     price = parseFloat(price);
     if (price < 1) {
-      options['minimumFractionDigits'] = 5;
+      options["minimumFractionDigits"] = 5;
     }
 
     return price.toLocaleString(undefined, options);
