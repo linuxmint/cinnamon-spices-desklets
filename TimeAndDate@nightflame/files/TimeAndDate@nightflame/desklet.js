532fe68fa2ad26f83d5271bdbb60704693eea365
Jason Hicks <jaszhix@gmail.com>
2019-03-24 04:12:07 -0500

    TimeAndDate@nightflame: Make compatible with Cinnamon#8424

--- TimeAndDate@nightflame/files/TimeAndDate@nightflame/desklet.js
+++ TimeAndDate@nightflame/files/TimeAndDate@nightflame/desklet.js
@@ -1,14 +1,15 @@
 
 // Time And Date Cinnamon Desklet v0.1 - 19 June 2013
 //
-// This is a simple desklet to display the time and date. The size and format of the date are configurable by changing the values in metadata.json. 
+// This is a simple desklet to display the time and date. The size and format of the date are configurable by changing the values in metadata.json.
 // This can be launched from the Desklet itself by selecting Config from the menu.
-// 
-// I'm sharing it in case it useful to anyone else especially as there do not seem to be many Cinammon Desklets yet. 
+//
+// I'm sharing it in case it useful to anyone else especially as there do not seem to be many Cinammon Desklets yet.
 //
 // -Steve
 // desklets [at] stargw [dot] eu
 
+const Cinnamon = imports.gi.Cinnamon;
 const Gio = imports.gi.Gio;
 const St = imports.gi.St;
 
@@ -31,6 +32,10 @@ function _(str) {
   return Gettext.dgettext(UUID, str);
 }
 
+const toLocaleFormat = function toLocaleFormat(date, format) {
+	return Cinnamon.util_format_date(format, date.getTime());
+};
+
 function MyDesklet(metadata){
     this._init(metadata);
 }
@@ -40,23 +45,23 @@ MyDesklet.prototype = {
 
 	_init: function(metadata){
 		Desklet.Desklet.prototype._init.call(this, metadata);
-		
+
 		this.metadata = metadata
 		this.dateFormat = this.metadata["dateFormat"];
 		this.dateSize = this.metadata["dateSize"];
 		this.timeFormat = this.metadata["timeFormat"];
 		this.timeSize = this.metadata["timeSize"];
 
-			
+
 		this._clockContainer = new St.BoxLayout({vertical:true, style_class: 'clock-container'});
-		
+
 		this._dateContainer =  new St.BoxLayout({vertical:false, style_class: 'date-container'});
 		this._timeContainer =  new St.BoxLayout({vertical:false, style_class: 'time-container'});
 
 		this._date = new St.Label();
 		this._time = new St.Label();
-		
-		
+
+
 		this._dateContainer.add(this._date);
 		this._timeContainer.add(this._time);
 
@@ -65,30 +70,30 @@ MyDesklet.prototype = {
 
 		this.setContent(this._clockContainer);
 		this.setHeader(_("Time And Date"));
-		
+
 		// Set the font sizes from .json file
-		
+
 		this._date.style="font-size: " + this.dateSize;
 		this._time.style="font-size: " + this.timeSize;
-		
+
 		// let dir_path = ;
 		// this.save_path = dir_path.replace('~', GLib.get_home_dir());
 		this.configFile = GLib.get_home_dir() + "/.local/share/cinnamon/desklets/TimeAndDate@nightflame/metadata.json";
 		this.helpFile = GLib.get_home_dir() + "/.local/share/cinnamon/desklets/TimeAndDate@nightflame/README";
-		
+
 		global.log("Config file " + this.configFile);
-		
+
 		this._menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
 		this._menu.addAction(_("Edit Config"), Lang.bind(this, function() {
 			Util.spawnCommandLine("xdg-open " + this.configFile);
 		}));
-		
+
 		this._menu.addAction(_("Help"), Lang.bind(this, function() {
 			Util.spawnCommandLine("xdg-open " + this.helpFile);
 		}));
-		
-		
+
+
 		this._updateDate();
 	},
 
@@ -102,12 +107,12 @@ MyDesklet.prototype = {
 		// let dateFormat = '%A,%e %B';
 		let displayDate = new Date();
 
-	
-		this._time.set_text(displayDate.toLocaleFormat(this.timeFormat));
-		this._date.set_text(displayDate.toLocaleFormat(this.dateFormat));
-		
+
+		this._time.set_text(toLocaleFormat(displayDate, this.timeFormat));
+		this._date.set_text(toLocaleFormat(displayDate, this.dateFormat));
+
 		this.timeout = Mainloop.timeout_add_seconds(1, Lang.bind(this, this._updateDate));
-		
+
 	}
 }
 
