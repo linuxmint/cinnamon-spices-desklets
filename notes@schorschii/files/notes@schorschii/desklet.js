c79e1a6b5d94cab0538ca04e2f92245b04ad1735
Georg Sieber <it@georg-sieber.de>
2019-11-16 16:54:59 +0100

    notes@schorschii code optimizations

--- notes@schorschii/files/notes@schorschii/desklet.js
+++ notes@schorschii/files/notes@schorschii/desklet.js
@@ -10,6 +10,7 @@ const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const GdkPixbuf = imports.gi.GdkPixbuf;
 const Cogl = imports.gi.Cogl;
+const Gio = imports.gi.Gio;
 
 const UUID = "notes@schorschii";
 const DESKLET_ROOT = imports.ui.deskletManager.deskletMeta[UUID].path;
@@ -82,56 +83,71 @@ MyDesklet.prototype = {
 
 		// load image index
 		this.imageIndex = new Array();
-		this.imageIndexRaw = Cinnamon.get_file_contents_utf8_sync(DESKLET_ROOT + '/img/index.csv');
-		let lines = this.imageIndexRaw.split('\n');
-		for(var i = 0;i < lines.length;i++) {
-			let fields = lines[i].split(',');
-			if(fields.length == 9) {
-				let style = new Array();
-				style['name'] = fields[0];
-				style['imagePath'] = fields[1];
-				style['marginTop'] = fields[2];
-				style['marginRight'] = fields[3];
-				style['marginBottom'] = fields[4];
-				style['marginLeft'] = fields[5];
-				style['sizeWidth'] = fields[6];
-				style['sizeHeight'] = fields[7];
-				style['defaultTextColor'] = fields[8];
-				this.imageIndex.push(style);
+		let file = Gio.file_new_for_path(DESKLET_ROOT + '/img/index.csv');
+		file.load_contents_async(null, (file, response) => {
+			try {
+				let [success, contents, tag] = file.load_contents_finish(response);
+				if(success) {
+					let lines = contents.toString().split('\n');
+					for(var i = 0;i < lines.length;i++) {
+						let fields = lines[i].split(',');
+						if(fields.length != 9) { continue; }
+						let style = new Array();
+						style['name'] = fields[0];
+						style['imagePath'] = fields[1];
+						style['marginTop'] = fields[2];
+						style['marginRight'] = fields[3];
+						style['marginBottom'] = fields[4];
+						style['marginLeft'] = fields[5];
+						style['sizeWidth'] = fields[6];
+						style['sizeHeight'] = fields[7];
+						style['defaultTextColor'] = fields[8];
+						this.imageIndex.push(style);
+					}
+				}
+				GLib.free(contents);
+			} catch(err) {
+				global.log(err.message);
 			}
-		}
 
-		// load images and set initial sizes
-		this.refreshSize(true);
+			// set initial values
+			this.loadText(true);
+		});
 
 		// set decoration settings
 		this.refreshDecoration();
-
-		// set initial values
-		this.refresh();
 	},
 
-	refresh: function() {
-		try {
-			// read notes text file
-			this.finalPath = decodeURIComponent(this.file.replace("file://", ""));
-			if(this.finalPath == "") this.finalPath = "note.txt"; // in home dir
+	loadText: function(reloadGraphics = false) {
+		// get notes text file path
+		this.finalPath = decodeURIComponent(this.file.replace("file://", ""));
+		if(this.finalPath == "") this.finalPath = "note.txt"; // in home dir
+		// read file async
+		let file = Gio.file_new_for_path(this.finalPath);
+		file.load_contents_async(null, (file, response) => {
+			try {
+				let [success, contents, tag] = file.load_contents_finish(response);
+				if(success) {
+					this.notecontent = contents.toString();
+				} else {
+					// error reading file - maybe the file does not exist
+					this.notecontent = _("Can't read text file.\nSelect a file in settings.\n\nClick here to edit.");
+				}
+				GLib.free(contents);
+			} catch(err) {
+				this.notecontent = err.message;
+			}
 
-			this.notecontent = Cinnamon.get_file_contents_utf8_sync(this.finalPath);
-		} catch(ex) {
-			// error reading file - maybe the file does not exist
-			this.notecontent = _("Can't read text file.\nSelect a file in settings.\n\nClick here to edit.");
-		}
-
-		// set object sizes without recalc
-		this.refreshSize(false);
+			// refresh desklet content
+			this.refreshDesklet(reloadGraphics);
+		});
 
 		// refresh again in two seconds
-		this.timeout = Mainloop.timeout_add_seconds(2, Lang.bind(this, this.refresh));
+		this.timeout = Mainloop.timeout_add_seconds(2, Lang.bind(this, this.loadText));
 	},
 
-	refreshSize: function(reloadGraphics = false) {
-		if (reloadGraphics == true) {
+	refreshDesklet: function(reloadGraphics = false) {
+		if(reloadGraphics) {
 
 			// set default image
 			this.bgImg = "none";
@@ -162,12 +178,13 @@ MyDesklet.prototype = {
 			}
 
 			// calc new sizes based on scale factor
-			this.desklet_width = this.size_width * this.scaleSize;
-			this.desklet_height = this.size_height * this.scaleSize;
-			this.label_top = this.default_notepad_label_top * this.scaleSize;
-			this.label_left = this.default_notepad_label_left * this.scaleSize;
-			this.label_right = this.default_notepad_label_right * this.scaleSize;
-			this.label_bottom = this.default_notepad_label_bottom * this.scaleSize;
+			let scale = this.scaleSize * global.ui_scale;
+			this.desklet_width = this.size_width * scale;
+			this.desklet_height = this.size_height * scale;
+			this.label_top = this.default_notepad_label_top * scale;
+			this.label_left = this.default_notepad_label_left * scale;
+			this.label_right = this.default_notepad_label_right * scale;
+			this.label_bottom = this.default_notepad_label_bottom * scale;
 
 			// create elements
 			this.notepad =
@@ -181,10 +198,10 @@ MyDesklet.prototype = {
 			this.notetext.set_position(Math.round(this.label_left), Math.round(this.label_top));
 			this.notetext.set_size(this.desklet_width - this.label_left - this.label_right, this.desklet_height - this.label_top - this.label_bottom);
 			this.notetext.style = "font-family: '" + this.font + "';"
-			                    + "font-size: " + this.sizeFont + "px;"
-			                    + "color:" + this.textColor + ";"
-			                    + "font-weight:" + (this.fontBold ? "bold" : "normal") + ";"
-			                    + "font-style:" + (this.fontItalic ? "italic" : "normal") + ";";
+								+ "font-size: " + this.sizeFont + "px;"
+								+ "color:" + this.textColor + ";"
+								+ "font-weight:" + (this.fontBold ? "bold" : "normal") + ";"
+								+ "font-style:" + (this.fontItalic ? "italic" : "normal") + ";";
 
 			// add actor
 			this.notepad.remove_all_children();
@@ -192,20 +209,18 @@ MyDesklet.prototype = {
 			this.container.add_actor(this.notetext);
 			this.setContent(this.notepad); // set root element
 
-			// debug
-			//Main.notifyError("Complete Refresh Done", " ");
+			//Main.notifyError("Complete Refresh Done", " "); // debug
 		}
 
 		// refresh text
 		this.notetext.set_text(this.notecontent);
 
-		// debug
-		//Main.notifyError("Text Refresh Done", " ");
+		//Main.notifyError("Text Refresh Done", " "); // debug
 	},
 
 	refreshDecoration: function() {
 		// desklet label (header)
-		if (this.useCustomLabel == true)
+		if(this.useCustomLabel == true)
 			this.setHeader(this.customLabel)
 		else
 			this.setHeader(_("Notepad"));
@@ -221,14 +236,11 @@ MyDesklet.prototype = {
 
 		// settings changed; instant refresh
 		Mainloop.source_remove(this.timeout);
-		this.refresh();
-
-		// update size based on scale factor
-		this.refreshSize(true);
+		this.loadText(true);
 	},
 
 	on_desklet_clicked: function() {
-		if (this.editCmd != "") {
+		if(this.editCmd != "") {
 			Util.spawnCommandLine(this.editCmd.replace("%f", '"'+this.finalPath+'"'));
 		}
 	},
