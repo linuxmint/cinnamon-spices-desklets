c339939c712a56feae899c83c5c0c6d359ae4229
Sujit Agarwal <sujitagarwal.com@gmail.com>
2020-04-21 00:06:54 +0530

    Add values from Covid-19 India API

--- covid-19-india@sujitagarwal/files/covid-19-india@sujitagarwal/desklet.js
+++ covid-19-india@sujitagarwal/files/covid-19-india@sujitagarwal/desklet.js
@@ -24,21 +24,25 @@ Covid19IndiaDesklet.prototype = {
         this.titles = new St.BoxLayout({vertical: true});
         this.values = new St.BoxLayout({vertical: true});
 
+        this.titleUpdated = new St.Label({text: _("Last Update : "), style_class: "CovidTitle"});
         this.titleConfirmed = new St.Label({text: _("Confirmed : "), style_class: "CovidTitle"});
         this.titleActive = new St.Label({text: _("Active : "), style_class: "CovidTitle"});
         this.titleRecovered = new St.Label({text: _("Recovered : "), style_class: "CovidTitle"});
         this.titleDeceased = new St.Label({text: _("Deceased : "), style_class: "CovidTitle"});
 
+        this.titles.add(this.titleUpdated);
         this.titles.add(this.titleConfirmed);
         this.titles.add(this.titleActive);
         this.titles.add(this.titleRecovered);
         this.titles.add(this.titleDeceased);
 
+        this.titleUpdatedValue = new St.Label({text: "loading...", style_class: "CovidBValue"});
         this.titleConfirmedValue = new St.Label({text: "loading...", style_class: "CovidBValue"});
         this.titleActiveValue = new St.Label({text: "loading...", style_class: "CovidBValue"});
         this.titleRecoveredValue = new St.Label({text: "loading...", style_class: "CovidBValue"});
         this.titleDeceasedValue = new St.Label({text: "loading...", style_class: "CovidBValue"});
 
+        this.values.add(this.titleUpdatedValue);
         this.values.add(this.titleConfirmedValue);
         this.values.add(this.titleActiveValue);
         this.values.add(this.titleRecoveredValue);
@@ -54,29 +58,28 @@ Covid19IndiaDesklet.prototype = {
 
         let message = Soup.Message.new('GET', url);
         _httpSession.send_message (message);
-        var match = "0";
+        var jsonData = "EMPTY";
         if (message.status_code!== Soup.KnownStatusCode.OK) {
             this._date.set_text("Server Unreachable.");
             var sleep = 30
         } else {
-            let jp = new Json.Parser();
-            jp.load_from_data(message.response_body.data.toString(), -1);
-            match=jp.get_root().get_object();
+            jsonData = JSON.parse(message.response_body.data.toString());
         }
-        return message.response_body.data.toString();
+        return jsonData.statewise[0];
     },
 
     refreshStats: function() {
         var jsonData = this.getJSON("https://api.covid19india.org/data.json");
-        this.titleConfirmedValue.set_text("hello");
-        this.titleActiveValue.set_text("hello");
-        this.titleRecoveredValue.set_text("hello");
-        this.titleDeceasedValue.set_text("hello");
+        this.titleUpdatedValue.set_text(jsonData.lastupdatedtime);
+        this.titleConfirmedValue.set_text(jsonData.confirmed);
+        this.titleActiveValue.set_text(jsonData.active);
+        this.titleRecoveredValue.set_text(jsonData.recovered);
+        this.titleDeceasedValue.set_text(jsonData.deaths);
     },
 
     _updateWidget: function(){
         this.refreshStats();
-        this.timeout = Mainloop.timeout_add_seconds(1, Lang.bind(this, this._updateWidget));
+        this.timeout = Mainloop.timeout_add_seconds(60, Lang.bind(this, this._updateWidget));
     },
 
     on_desklet_removed: function() {
