cbcfe81bf28e491d34c4b9f13a3565172b9f0c4a
Rico <mailvonrico@gmail.com>
2024-11-08 20:08:42 +0100

    refactor

--- systemTemperature@KopfDesDaemons/README.md
+++ systemTemperature@KopfDesDaemons/README.md
@@ -21,22 +21,3 @@ This desklet displays the temperature of a specific thermal zone on your system.
    In the desklet settings, specify the full path to the temperature file you want to monitor, such as:
 
    `/sys/class/thermal/thermal_zone2/temp`
-
-   This will allow the desklet to display the correct temperature for the chosen thermal zone.
-
-## Development
-
-Importent commands for the desklet development:
-
-Open desklets error log:
-
-```bash
- tail -f ~/.xsession-errors
-```
-
-Install translation:
-
-```bash
-cd ~/.local/share/cinnamon/desklets/devtest-systemTemperature@KopfDesDaemons
-cinnamon-json-makepot -i
-```
--- systemTemperature@KopfDesDaemons/files/systemTemperature@KopfDesDaemons/desklet.js
+++ systemTemperature@KopfDesDaemons/files/systemTemperature@KopfDesDaemons/desklet.js
@@ -8,7 +8,7 @@ const Gettext = imports.gettext;
 
 const UUID = "systemTemperature@KopfDesDaemons";
 
-Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale")
+Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
 
 function _(str) {
     return Gettext.dgettext(UUID, str);
@@ -24,8 +24,10 @@ TemperatureDesklet.prototype = {
     _init: function (metadata, deskletId) {
         Desklet.Desklet.prototype._init.call(this, metadata, deskletId);
 
+        this.setHeader(_("System Temperature"));
+
         // Initialize settings
-        this.settings = new Settings.DeskletSettings(this, metadata["uuid"], deskletId);
+        this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], deskletId);
 
         // Get settings
         this.initialLabelText = this.settings.getValue("labelText") || "CPU temperature:";
@@ -37,13 +39,18 @@ TemperatureDesklet.prototype = {
         this.updateInterval = this.settings.getValue("updateInterval") || 1;
 
         // Bind settings properties
-        this.settings.bindProperty(Settings.BindingDirection.IN, "tempFilePath", "tempFilePath", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "labelText", "labelText", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "temperatureUnit", "temperatureUnit", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "updateInterval", "updateInterval", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "fontSizeLabel", "fontSizeLabel", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "fontSizeTemperature", "fontSizeTemperature", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "dynamicColorEnabled", "dynamicColorEnabled", this.on_settings_changed, null);
+        const boundSettings = [
+            "tempFilePath",
+            "labelText",
+            "temperatureUnit",
+            "updateInterval",
+            "fontSizeLabel",
+            "fontSizeTemperature",
+            "dynamicColorEnabled"
+        ];
+        boundSettings.forEach(setting => {
+            this.settings.bindProperty(Settings.BindingDirection.IN, setting, setting, this.on_settings_changed, null);
+        });
 
         // Create label for the static text
         this.label = new St.Label({ text: this.initialLabelText, y_align: St.Align.START, style_class: "label-text" });
@@ -67,65 +74,58 @@ TemperatureDesklet.prototype = {
 
     updateTemperature: function () {
         try {
-            let [result, out] = GLib.spawn_command_line_sync("cat " + this.tempFilePath);
+            // Get CPU temperature
+            const [result, out] = GLib.spawn_command_line_sync(`cat ${this.tempFilePath}`);
 
-            if (result && out !== null) {
-                // Convert temperature from millidegree Celsius to degree Celsius
-                let temperature = parseFloat(out.toString().trim()) / 1000.0;
-
-                // Convert to Fahrenheit if needed
-                if (this.temperatureUnit === "F") {
-                    temperature = (temperature * 9 / 5) + 32;
-                }
-
-                // Update temperature text with the chosen unit
-                this.temperatureLabel.set_text(temperature.toFixed(1) + "°" + this.temperatureUnit);
-
-                // Set the color based on the temperature if dynamic color is enabled
-                if (this.dynamicColorEnabled) {
-                    this.updateLabelColor(temperature);
-                } else {
-                    // Set default color if dynamic color is disabled
-                    this.temperatureLabel.set_style(`color: #ffffff; font-size: ${this.fontSizeTemperature}px;`);
-                }
-            } else {
-                this.temperatureLabel.set_text("Error");
-                global.logError("Error: Could not retrieve CPU temperature.");
+            if (!result || out === null) {
+                throw new Error("Could not retrieve CPU temperature.");
             }
+
+            // Convert temperature from millidegree Celsius to degree Celsius
+            let temperature = parseFloat(out.toString().trim()) / 1000.0;
+            if (this.temperatureUnit === "F") {
+                temperature = (temperature * 9 / 5) + 32;
+            }
+
+            // Update temperature text with the chosen unit
+            const temperatureText = `${temperature.toFixed(1)}°${this.temperatureUnit}`;
+            this.temperatureLabel.set_text(temperatureText);
+
+            // Set color based on temperature if dynamic color is enabled, else set default color
+            if (this.dynamicColorEnabled) {
+                this.updateLabelColor(temperature);
+            } else {
+                this.temperatureLabel.set_style(`color: #ffffff; font-size: ${this.fontSizeTemperature}px;`);
+            }
+
         } catch (e) {
-            global.logError("Error in updateTemperature: " + e.message);
             this.temperatureLabel.set_text("Error");
+            global.logError(`Error in updateTemperature: ${e.message}`);
         }
 
-        if (this._timeout) {
-            Mainloop.source_remove(this._timeout);
-        }
-        this._timeout = Mainloop.timeout_add_seconds(this.updateInterval, Lang.bind(this, this.updateTemperature));
+        // Reset and set up the interval timeout
+        if (this._timeout) Mainloop.source_remove(this._timeout);
+        this._timeout = Mainloop.timeout_add_seconds(this.updateInterval, () => this.updateTemperature());
     },
 
     updateLabelColor: function (temperature) {
-        // Limits for color change depending on the temperature unit
-        let minTemp, maxTemp;
+        // Define min and max temperature thresholds based on the unit
+        let minTemp = 20, maxTemp = 90;
 
+        // Convert min and max temperature from degree Celsius to degree Fahrenheit
         if (this.temperatureUnit === "F") {
-            minTemp = (20 * 9 / 5) + 32;
-            maxTemp = (90 * 9 / 5) + 32;
-        } else {
-            // Celsius Limits
-            minTemp = 20;
-            maxTemp = 90;
+            minTemp = (minTemp * 9 / 5) + 32;
+            maxTemp = (maxTemp * 9 / 5) + 32;
         }
 
-        temperature = Math.max(minTemp, Math.min(maxTemp, temperature));
-
-        let green = Math.max(0, 255 - Math.floor((temperature - minTemp) / (maxTemp - minTemp) * 255));
-        let red = Math.max(0, Math.floor((temperature - minTemp) / (maxTemp - minTemp) * 255));
+        // Calculate color based on temperature
+        temperature = Math.min(maxTemp, Math.max(minTemp, temperature));
+        const ratio = (temperature - minTemp) / (maxTemp - minTemp);
+        let color = `rgb(${Math.floor(ratio * 255)}, ${Math.floor((1 - ratio) * 255)}, 0)`;
 
         // Set the color
-        let color = `rgb(${red}, ${green}, 0)`;
         this.temperatureLabel.set_style(`color: ${color}; font-size: ${this.fontSizeTemperature}px;`);
     },
-
 
     on_settings_changed: function () {
         // Update the label text and styles when the settings change
