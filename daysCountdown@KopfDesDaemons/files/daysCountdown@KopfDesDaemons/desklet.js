7d1555c4204aef8ec12127c33fdad6e4efedd5c3
Rico <mailvonrico@gmail.com>
2024-11-24 13:46:13 +0100

    refresh setting, new screenshot, compress icon

--- daysCountdown@KopfDesDaemons/files/daysCountdown@KopfDesDaemons/desklet.js
+++ daysCountdown@KopfDesDaemons/files/daysCountdown@KopfDesDaemons/desklet.js
@@ -30,32 +30,35 @@ MyDesklet.prototype = {
         this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], deskletId);
 
         // Get settings
-        this.labelText = this.settings.getValue("labelText") || "New Year Countdown";
+        this.labelText = this.settings.getValue("labelText") || "New Year 2025 Countdown";
         this.fontSizeLabel = this.settings.getValue("fontSizeLabel") || 12;
         this.fontSizeCountdown = this.settings.getValue("fontSizeCountdown") || 36;
         this.colorCountdown = this.settings.getValue("colorCountdown") || "rgb(255, 255, 255)";
-        this.colorLabel = this.settings.getValue("colorLabel") || "rgb(255, 255, 255)";
-        this.countdownDate = this.settings.getValue("countdownDate") || {
-            d: 31,
-            m: 12,
-            y: new Date().getFullYear()
-        };
+        this.colorLabel = this.settings.getValue("colorLabel") || "rgb(98, 160, 234)";
+        this.countdownDate = this.settings.getValue("countdownDate") || { d: 31, m: 12, y: 2025 };
+        this.refreshInterval = this.settings.getValue("refreshInterval") || "only-after-starting";
 
         // Bind settings properties
-        this.settings.bindProperty(Settings.BindingDirection.IN, "labelText", "labelText", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "countdownDate", "countdownDate", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "fontSizeLabel", "fontSizeLabel", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "fontSizeCountdown", "fontSizeCountdown", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "colorLabel", "colorLabel", this.on_settings_changed, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "colorCountdown", "colorCountdown", this.on_settings_changed, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "labelText", "labelText", this.updateUI, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "countdownDate", "countdownDate", this.updateUI, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "fontSizeLabel", "fontSizeLabel", this.updateUI, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "fontSizeCountdown", "fontSizeCountdown", this.updateUI, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "colorLabel", "colorLabel", this.updateUI, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "colorCountdown", "colorCountdown", this.updateUI, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "refreshInterval", "refreshInterval", this.refreshCountdown, null);
 
         // Set up the layout
         const box = new St.BoxLayout({ vertical: true });
-        this.textLabel = new St.Label({ text: this.labelText, style_class: "label-text", style: `font-size: ${this.fontSizeLabel}px; color: ${this.colorLabel};` });
-        this.daysLabel = new St.Label({ text: this.getDaysString(), style_class: "days-label", style: `font-size: ${this.fontSizeCountdown}px; color: ${this.colorCountdown};` });
+        this.textLabel = new St.Label();
+        this.daysLabel = new St.Label();
         box.add_child(this.textLabel);
         box.add_child(this.daysLabel);
         this.setContent(box);
+
+        this.timeout = null;
+
+        this.updateUI();
+        this.refreshCountdown();
     },
 
     calcDays() {
@@ -73,7 +76,7 @@ MyDesklet.prototype = {
         return daysString.format(days);
     },
 
-    on_settings_changed: function () {
+    updateUI: function () {
         // Update the label text and styles when the settings change
         if (this.textLabel && this.labelText) {
             this.textLabel.set_text(this.labelText);
@@ -85,6 +88,32 @@ MyDesklet.prototype = {
             this.daysLabel.set_style(`font-size: ${this.fontSizeCountdown}px; color: ${this.colorCountdown};`);
         }
     },
+
+    refreshCountdown: function () {
+        this.daysLabel.set_text(this.getDaysString());
+        if (this.timeout) Mainloop.source_remove(this.timeout);
+        if (this.refreshInterval === "only-after-starting") return;
+        global.log("daysCountdown@KopfDesDaemons: " + this.labelText + " refreshed: Next refresh in " + this.refreshInterval + " seconds.");
+        this.timeout = Mainloop.timeout_add_seconds(this.refreshInterval, () => this.refreshCountdown());
+    },
+
+    on_desklet_removed: function () {
+        // Clean up the timeout when the desklet is removed
+        if (this.timeout) {
+            Mainloop.source_remove(this.timeout);
+            this.timeout = null;
+        }
+
+        if (this.textLabel) {
+            this.box.remove_child(this.textLabel);
+            this.textLabel = null;
+        }
+
+        if (this.daysLabel) {
+            this.box.remove_child(this.daysLabel);
+            this.daysLabel = null;
+        }
+    }
 };
 
 function main(metadata, deskletId) {
