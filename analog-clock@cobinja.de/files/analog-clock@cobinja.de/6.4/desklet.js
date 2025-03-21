7452db70e61015dc57bc6b167bf9bb7a83a19f2f
claudiux <claude.clerc@gmail.com>
2025-03-21 17:58:56 +0100

    analog-clock@cobinja.de: Code cleanup as requested by Author

--- analog-clock@cobinja.de/files/analog-clock@cobinja.de/6.4/desklet.js
+++ analog-clock@cobinja.de/files/analog-clock@cobinja.de/6.4/desklet.js
@@ -15,14 +15,7 @@ const Clutter = imports.gi.Clutter;
 const Cogl = imports.gi.Cogl;
 const SignalManager = imports.misc.signalManager;
 const {
-  _sourceIds,
   timeout_add_seconds,
-  timeout_add,
-  setTimeout,
-  clearTimeout,
-  setInterval,
-  clearInterval,
-  source_exists,
   source_remove,
   remove_all_sources
 } = require("./mainloopTools")
@@ -38,10 +31,10 @@ const MARGIN = 5;
 function getImageAtScale(imageFileName, scale) {
   let width, height, fileInfo;
   [fileInfo, width, height] = GdkPixbuf.Pixbuf.get_file_info(imageFileName);
-
+  
   let scaledWidth = scale * width;
   let scaledHeight = scale * height;
-
+  
   let pixBuf = GdkPixbuf.Pixbuf.new_from_file_at_size(imageFileName, scaledWidth, scaledHeight);
   let image = new Clutter.Image();
   image.set_data(
@@ -50,10 +43,10 @@ function getImageAtScale(imageFileName, scale) {
     scaledWidth, scaledHeight,
     pixBuf.get_rowstride()
   );
-
+  
   let actor = new Clutter.Actor({width: scaledWidth, height: scaledHeight});
   actor.set_content(image);
-
+  
   return {actor: actor, origWidth: width, origHeight: height};
 }
 
@@ -66,7 +59,7 @@ CobiAnalogClockSettings.prototype = {
     this._instanceId = instanceId;
     this._signalManager = new SignalManager.SignalManager(null);
     this.values = {};
-
+    
     let settingsDirName = GLib.get_user_config_dir();
     if (!settingsDirName) {
       settingsDirName = GLib.get_home_dir() + "/.config";
@@ -76,24 +69,24 @@ CobiAnalogClockSettings.prototype = {
     if (!this._settingsDir.query_exists(null)) {
       this._settingsDir.make_directory_with_parents(null);
     }
-
+    
     this._settingsFile = this._settingsDir.get_child(this._instanceId + ".json");
     if (!this._settingsFile.query_exists(null)) {
       this._getDefaultSettingsFile().copy(this._settingsFile, 0, null, null);
     }
-
+    
     this._onSettingsChanged();
-
+    
     this._upgradeSettings();
-
+    
     this._monitor = this._settingsFile.monitor(Gio.FileMonitorFlags.NONE, null);
     this._signalManager.connect(this._monitor, "changed", this._onSettingsChanged, this);
   },
-
+  
   _getDefaultSettingsFile: function() {
     return Gio.file_new_for_path(DESKLET_DIR + "/default_settings.json");
   },
-
+  
   _onSettingsChanged: function() {
     let settings;
     try {
@@ -103,7 +96,7 @@ CobiAnalogClockSettings.prototype = {
       global.logError("Could not parse CobiAnalogClock's settings.json", e)
       return true;
     }
-
+    
     for (let key in settings) {
       if (settings.hasOwnProperty(key)) {
         let comparison;
@@ -132,7 +125,7 @@ CobiAnalogClockSettings.prototype = {
     }
     return true;
   },
-
+  
   _upgradeSettings: function() {
     let defaultSettings;
     try {
@@ -155,7 +148,7 @@ CobiAnalogClockSettings.prototype = {
     this._writeSettings();
     return false;
   },
-
+    
   setValue: function(key, value) {
     if (!compareArray(value, this.values[key])) {
       this.values[key] = value;
@@ -163,12 +156,12 @@ CobiAnalogClockSettings.prototype = {
       this._writeSettings();
     }
   },
-
+  
   _writeSettings: function() {
     let filedata = JSON.stringify(this.values, null, "  ");
     GLib.file_set_contents(this._settingsFile.get_path(), filedata);
   },
-
+  
   destroy: function(deleteConfig) {
     this._signalManager.disconnectAllSignals();
     this._monitor.cancel();
@@ -197,41 +190,41 @@ CobiAnalogClock.prototype = {
     Desklet.Desklet.prototype._init.call(this, metadata, instanceId);
     this._signalManager = new SignalManager.SignalManager(null);
     this._settings = new CobiAnalogClockSettings(instanceId);
-
+    
     this._displayTime = new GLib.DateTime();
 
     this._menu.addAction(_("Settings"), () => {Util.spawnCommandLine(DESKLET_DIR + "/settings.py " + instanceId);});
   },
-
+  
   on_desklet_added_to_desktop: function(userEnabled) {
     this.metadata["prevent-decorations"] = this._settings.values["hide-decorations"];
     this._updateDecoration();
-
+    
     this._clockSize = this._settings.values["size"] * global.ui_scale;
-
+    
     this._clockActor = new St.Widget();
-
+    
     this._tzLabel = new St.Label();
-
+    
     this.setHeader(_("Clock"));
     this.setContent(this._clockActor);
-
+    
     this._onSizeChanged();
-
+    
     let currentMillis = new Date().getMilliseconds();
     let timeoutMillis = (1000 - currentMillis) % 1000;
-
+    
     this._signalManager.connect(global, "scale-changed", this._onSizeChanged, this);
     this._signalManager.connect(this._settings, "size-changed", this._onSizeChanged, this);
     this._signalManager.connect(this._settings, "theme-changed", this._onThemeChanged, this);
     this._signalManager.connect(this._settings, "hide-decorations-changed", this._onHideDecorationsChanged, this);
     this._signalManager.connect(global.settings, "changed::desklet-decorations", this._onHideDecorationsChanged, this);
     this._signalManager.connect(this._settings, "show-seconds-changed", this._onShowSecondsChanged, this);
-
+    
     this._signalManager.connect(this._settings, "timezone-use-changed", this._onTimezoneChanged, this);
     this._signalManager.connect(this._settings, "timezone-changed", this._onTimezoneChanged, this);
     this._signalManager.connect(this._settings, "timezone-display-changed", this._onTimezoneDisplayChanged, this);
-
+    
     this._upClient = new UPowerGlib.Client();
     try {
       this._upClient.connect('notify-resume', () => { this._onPowerResume() });
@@ -241,72 +234,72 @@ CobiAnalogClock.prototype = {
     }
     this._onTimezoneChanged();
   },
-
+  
   _onPowerResume: function() {
     this._initialUpdate = true;
     this._updateClock();
   },
-
+  
   _loadTheme: function() {
     let themeName = this._settings.values["theme"];
     let themesDir = Gio.file_new_for_path(DESKLET_DIR + "/../themes");
     let themeDir = themesDir.get_child(themeName);
     let metaDataFile = themeDir.get_child("metadata.json");
     let metaData = JSON.parse(Cinnamon.get_file_contents_utf8_sync(metaDataFile.get_path()));
-
+    
     let clock = {"size": metaData["size"], "tz-label": metaData["tz-label"]};
     let scale = this._clockSize / clock["size"];
-
+    
     let bodyFileName = metaData["body"];
     let body = getImageAtScale(themeDir.get_child(bodyFileName).get_path(), scale);
     clock.body = body;
-
+    
     let clockfaceFileName = metaData["clockface"];
     let clockface = getImageAtScale(themeDir.get_child(clockfaceFileName).get_path(), scale);
     clock.clockface = clockface;
-
+    
     let frameFileName = metaData["frame"];
     let frame = getImageAtScale(themeDir.get_child(frameFileName).get_path(), scale);
     clock.frame = frame;
-
+    
     let hourFileName = metaData["hour"]["fileName"];
     let hour = getImageAtScale(themeDir.get_child(hourFileName).get_path(), scale);
     hour.pivotX = metaData["hour"]["pivot-x"];
     hour.pivotY = metaData["hour"]["pivot-y"];
     clock.hour = hour;
-
+    
     let minuteFileName = metaData["minute"]["fileName"];
     let minute = getImageAtScale(themeDir.get_child(minuteFileName).get_path(), scale);
     minute.pivotX = metaData["minute"]["pivot-x"];
     minute.pivotY = metaData["minute"]["pivot-y"];
     clock.minute = minute;
-
+    
     let secondFileName = metaData["second"]["fileName"];
     let second = getImageAtScale(themeDir.get_child(secondFileName).get_path(), scale);
     second.pivotX = metaData["second"]["pivot-x"];
     second.pivotY = metaData["second"]["pivot-y"];
     clock.second = second;
-
+    
     return clock;
   },
-
+  
   _loadClock: function() {
     let newClock = this._loadTheme();
     this._clock = newClock;
     this._clockActor.remove_all_children();
-
+    
     this._clockActor.set_style(this._clock["tz-label"]);
     this._clockActor.add_actor(this._clock.body.actor);
     this._clock.body.actor.set_position(MARGIN * global.ui_scale, MARGIN * global.ui_scale);
-
+    
     this._clockActor.add_actor(this._clock.clockface.actor);
     this._clock.clockface.actor.set_position(MARGIN * global.ui_scale, MARGIN * global.ui_scale);
-
+    
     // add timezone label
     this._clockActor.add_actor(this._tzLabel);
     //this._tzLabel.set_style(this._clock["tz-label"]);
     this._updateTzLabel();
-
+    
     // add hands
     let hour = this._clock.hour;
     this._clockActor.add_actor(hour.actor);
@@ -316,7 +309,7 @@ CobiAnalogClock.prototype = {
     hour.actor.set_pivot_point(pivotPoint.x, pivotPoint.y);
     hour.actor.set_position(((this._clockSize / 2) - pivotPoint.x * hour.actor.size.width) + MARGIN * global.ui_scale,
                                         (this._clockSize / 2) - pivotPoint.y * hour.actor.size.height + MARGIN * global.ui_scale);
-
+    
     let minute = this._clock.minute;
     this._clockActor.add_actor(minute.actor);
     pivotPoint.x = minute.pivotX / minute.origWidth;
@@ -324,7 +317,7 @@ CobiAnalogClock.prototype = {
     minute.actor.set_pivot_point(pivotPoint.x, pivotPoint.y);
     minute.actor.set_position((this._clockSize / 2) - pivotPoint.x * minute.actor.size.width + MARGIN * global.ui_scale,
                                           (this._clockSize / 2) - pivotPoint.y * minute.actor.size.height + MARGIN * global.ui_scale);
-
+    
     let second = this._clock.second;
     this._clockActor.add_actor(second.actor);
     pivotPoint.x = second.pivotX / second.origWidth;
@@ -333,13 +326,13 @@ CobiAnalogClock.prototype = {
     second.actor.set_position((this._clockSize / 2) - pivotPoint.x * second.actor.size.width + MARGIN * global.ui_scale,
                                           (this._clockSize / 2) - pivotPoint.y * second.actor.size.height + MARGIN * global.ui_scale);
     this._showSecondsChangedImpl();
-
+    
     this._clockActor.add_actor(this._clock.frame.actor);
     this._clock.frame.actor.set_position(MARGIN * global.ui_scale, MARGIN * global.ui_scale);
-
+    
     this._initialUpdate = true;
   },
-
+  
   _onThemeChanged: function() {
     if (this._timeoutId)
       source_remove(this._timeoutId);
@@ -351,7 +344,7 @@ CobiAnalogClock.prototype = {
     }
     this._updateClock();
   },
-
+  
   _onSizeChanged: function() {
     let size = this._settings.values["size"] * global.ui_scale;
     this._clockActor.set_width(size + 2 * MARGIN * global.ui_scale);
@@ -360,23 +353,23 @@ CobiAnalogClock.prototype = {
     this._loadClock();
     this._updateClock();
   },
-
+  
   _showSecondsChangedImpl: function() {
     let showSeconds = this._settings.values["show-seconds"];
     showSeconds ? this._clock.second.actor.show() : this._clock.second.actor.hide();
   },
-
+  
   _onShowSecondsChanged: function() {
     this._showSecondsChangedImpl();
     this._updateClock();
   },
-
+  
   _onHideDecorationsChanged: function() {
     this.metadata["prevent-decorations"] = this._settings.values["hide-decorations"];
     this._updateDecoration();
     this._updateTzLabel();
   },
-
+  
   _onTimezoneChanged: function() {
     let tz = this._settings.values["timezone"];
     let zoneName = tz["region"];
@@ -397,12 +390,12 @@ CobiAnalogClock.prototype = {
     this._initialUpdate = true;
     this._updateClock();
   },
-
+  
   _onTimezoneDisplayChanged: function() {
     this._updateHeader();
     this._updateTzLabel();
   },
-
+  
   _getTzLabelText: function() {
     let result = _("Clock");
     if (this._settings.values["timezone-use"] && this._settings.values["timezone-display"]) {
@@ -416,7 +409,7 @@ CobiAnalogClock.prototype = {
     }
     return result;
   },
-
+  
   _updateTzLabel: function() {
     let showLabel = (this._settings.values["hide-decorations"] || global.settings.get_int("desklet-decorations") <= 1) &&
                      this._settings.values["timezone-use"] &&
@@ -437,11 +430,11 @@ CobiAnalogClock.prototype = {
       this._tzLabel.hide();
     }
   },
-
+  
   _updateHeader: function() {
     this.setHeader(this._getTzLabelText());
   },
-
+  
   _updateClock: function() {
     if (this._inRemoval != undefined) {
       return false;
@@ -451,17 +444,17 @@ CobiAnalogClock.prototype = {
       let tz = GLib.TimeZone.new(this._tzId);
       this._displayTime = this._displayTime.to_timezone(tz);
     }
-
+    
     let newTimeoutSeconds = 1;
     if (!this._settings.values["show-seconds"]) {
       let seconds = this._displayTime.get_second();
       newTimeoutSeconds = 60 - seconds;
     }
-
+    
     let hours = this._displayTime.get_hour() % 12;
     let minutes = this._displayTime.get_minute() % 60;
     let seconds = this._displayTime.get_second() % 60;
-
+    
     if (seconds == 0 || this._initialUpdate) {
       if (minutes % 2 == 0 || this._initialUpdate) {
         this._clock.hour.actor.set_rotation_angle(Clutter.RotateAxis.Z_AXIS, DEG_PER_HOUR * hours + (minutes * 0.5));
@@ -472,11 +465,11 @@ CobiAnalogClock.prototype = {
     if (this._settings.values["show-seconds"]) {
       this._clock.second.actor.set_rotation_angle(Clutter.RotateAxis.Z_AXIS, DEG_PER_SECOND * seconds);
     }
-
-    this._timeoutId = timeout_add_seconds(newTimeoutSeconds, () => { this._updateClock() });
+    
+    this._timeoutId = Mainloop.timeout_add_seconds(newTimeoutSeconds, Lang.bind(this, this._updateClock));
     return false;
   },
-
+  
   on_desklet_removed: function(deleteConfig) {
     this._inRemoval = true;
     if (this._timeoutId) {
