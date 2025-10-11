f7c9deda523e6bc6cd64d53bfc09ae4841fde18d
Rico <mailvonrico@gmail.com>
2025-10-12 00:54:48 +0200

    display contributionCount

--- github-contribution-grid@KopfdesDaemons/files/github-contribution-grid@KopfdesDaemons/desklet.js
+++ github-contribution-grid@KopfdesDaemons/files/github-contribution-grid@KopfdesDaemons/desklet.js
@@ -7,6 +7,7 @@ const Settings = imports.ui.settings;
 const UUID = "github-contribution-grid@KopfdesDaemons";
 
 const { GitHubHelper } = require("./helpers/github.helper");
+const { Day } = require("./models/day.model");
 
 Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
 
@@ -36,9 +37,37 @@ class MyDesklet extends Desklet.Desklet {
   }
 
   async _setupLayout() {
-    if (!this.githubUsername || !this.githubToken) return;
+    if (!this.githubUsername || !this.githubToken) {
+      this.setContent(new St.Label({ text: _("Please configure username and token in settings.") }));
+      global.log("setup layout...");
+      return;
+    }
 
-    const weeks = await GitHubHelper.getContributionData(this.githubUsername, this.githubToken);
+    try {
+      const weeks = await GitHubHelper.getContributionData(this.githubUsername, this.githubToken);
+
+      const days = [];
+
+      for (const week of weeks) {
+        const contributionDays = week.contributionDays;
+        for (const day of contributionDays) {
+          const date = day.date;
+          const contributionCount = day.contributionCount;
+
+          days.push(new Day(date, contributionCount));
+        }
+      }
+
+      const box = new St.BoxLayout({ vertical: true, style_class: "main-container" });
+      for (const day of days) {
+        const label = new St.Label({ text: `${day.contributionCount}` });
+        box.add_child(label);
+      }
+      this.setContent(box);
+    } catch (e) {
+      global.logError(`[${UUID}] Error fetching contribution data: ${e}`);
+      this.setContent(new St.Label({ text: _("Error fetching data. See logs for details.") }));
+    }
   }
 }
 
---
+++ github-contribution-grid@KopfdesDaemons/files/github-contribution-grid@KopfdesDaemons/models/day.model.js
@@ -0,0 +1,9 @@
+class Day {
+  date;
+  contributionCount;
+
+  constructor(date, contributionCount) {
+    this.date = date;
+    this.contributionCount = contributionCount;
+  }
+}
