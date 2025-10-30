0ef4a870db21d678258fd0066e1c2e29e8d2c94f
Rico <mailvonrico@gmail.com>
2025-10-30 19:01:46 +0100

    sun + moon

---
+++ day-percentage-bot@FlyingSaturn/README.md
@@ -0,0 +1,2 @@
+https://iconscout.com/free-icon/free-sun-icon_1798521
+https://iconscout.com/free-icon/free-moon-icon_1798529
--- day-percentage-bot@FlyingSaturn/files/day-percentage-bot@FlyingSaturn/desklet.js
+++ day-percentage-bot@FlyingSaturn/files/day-percentage-bot@FlyingSaturn/desklet.js
@@ -4,6 +4,9 @@ const Mainloop = imports.mainloop;
 const Lang = imports.lang;
 const GLib = imports.gi.GLib;
 const Gettext = imports.gettext;
+const GdkPixbuf = imports.gi.GdkPixbuf;
+const Clutter = imports.gi.Clutter;
+const Cogl = imports.gi.Cogl;
 
 const UUID = "day-percentage-bot@FlyingSaturn";
 
@@ -16,6 +19,7 @@ function _(str) {
 class MyDesklet extends Desklet.Desklet {
   constructor(metadata, deskletId) {
     super(metadata, deskletId);
+    this.metadata = metadata;
 
     this.setHeader(_("Day percentage"));
     this._setupUI();
@@ -23,12 +27,32 @@ class MyDesklet extends Desklet.Desklet {
   }
 
   _setupUI() {
-    this.window = new St.Bin();
+    this.container = new St.Group();
+
     this.text = new St.Label();
     this.text.set_text("... %");
+    this.text.style = "font-size: 24px; text-align: center;";
 
-    this.window.add_actor(this.text);
-    this.setContent(this.window);
+    const iconSize = 32;
+    const deskletPath = this.metadata.path;
+
+    try {
+      this.sunIcon = this._getImageAtScale(`${deskletPath}/images/sun.svg`, iconSize, iconSize);
+      this.moonIcon = this._getImageAtScale(`${deskletPath}/images/moon.svg`, iconSize, iconSize);
+    } catch (e) {
+      global.logError(`[${UUID}] Error loading icons: ${e}`);
+      this.sunIcon = new St.Label({ text: "S" });
+      this.moonIcon = new St.Label({ text: "M" });
+    }
+
+    this.sunIcon.set_pivot_point(0.5, 0.5);
+    this.moonIcon.set_pivot_point(0.5, 0.5);
+
+    this.container.add_actor(this.text);
+    this.container.add_actor(this.sunIcon);
+    this.container.add_actor(this.moonIcon);
+
+    this.setContent(this.container);
   }
 
   _update() {
@@ -37,7 +61,31 @@ class MyDesklet extends Desklet.Desklet {
     }
 
     const dayPercent = this._calcPercent();
+    const now = new Date();
+    const hour = now.getHours();
+
     this.text.set_text(dayPercent.toFixed(1) + " %");
+
+    // Position icon in a semi-circle
+    const radius = 80;
+    const textWidth = this.text.get_width();
+    const textHeight = this.text.get_height();
+    const centerX = textWidth / 2;
+    const centerY = textHeight / 2;
+
+    // Angle from 180 (left) to 0 (right) degrees
+    const angle = Math.PI * (dayPercent / 100.0);
+
+    const iconX = centerX - radius * Math.cos(angle);
+    const iconY = centerY - radius * Math.pow(Math.sin(angle), 0.7) + 10 * (1 - Math.sin(angle));
+
+    const isDay = hour >= 6 && hour < 18;
+    this.sunIcon.visible = isDay;
+    this.moonIcon.visible = !isDay;
+
+    const currentIcon = isDay ? this.sunIcon : this.moonIcon;
+    currentIcon.set_position(iconX - currentIcon.get_width() / 2, iconY - currentIcon.get_height() / 2);
+    this.text.set_position(0, currentIcon.get_height() / 2);
 
     // update every second
     this.timeout = Mainloop.timeout_add_seconds(1, Lang.bind(this, this._update));
@@ -51,10 +99,29 @@ class MyDesklet extends Desklet.Desklet {
     return (secondsPassed / totalSecondsInDay) * 100;
   }
 
+  _createActorFromPixbuf(pixBuf) {
+    const pixelFormat = pixBuf.get_has_alpha() ? Cogl.PixelFormat.RGBA_8888 : Cogl.PixelFormat.RGB_888;
+    const image = new Clutter.Image();
+    image.set_data(pixBuf.get_pixels(), pixelFormat, pixBuf.get_width(), pixBuf.get_height(), pixBuf.get_rowstride());
+
+    return new Clutter.Actor({
+      content: image,
+      width: pixBuf.get_width(),
+      height: pixBuf.get_height(),
+    });
+  }
+
+  _getImageAtScale(imageFilePath, requestedWidth, requestedHeight) {
+    const pixBuf = GdkPixbuf.Pixbuf.new_from_file_at_size(imageFilePath, requestedWidth, requestedHeight);
+    return this._createActorFromPixbuf(pixBuf);
+  }
+
   on_desklet_removed() {
     if (this.timeout) {
       Mainloop.source_remove(this.timeout);
     }
+    this.container.destroy_all_children();
+    this.container.destroy();
   }
 }
 
