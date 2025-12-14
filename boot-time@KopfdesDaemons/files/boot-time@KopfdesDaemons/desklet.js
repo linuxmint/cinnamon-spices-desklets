31f252576605756b4027a53daad1c24cf52e970b
Rico <mailvonrico@gmail.com>
2025-12-15 00:05:23 +0100

    settings

--- boot-time@KopfdesDaemons/files/boot-time@KopfdesDaemons/desklet.js
+++ boot-time@KopfdesDaemons/files/boot-time@KopfdesDaemons/desklet.js
@@ -6,6 +6,7 @@ const Mainloop = imports.mainloop;
 const Clutter = imports.gi.Clutter;
 const Cairo = imports.cairo;
 const Pango = imports.gi.Pango;
+const Settings = imports.ui.settings;
 
 const UUID = "boot-time@KopfdesDaemons";
 
@@ -20,19 +21,34 @@ const { BootTimeHelper } = require("./helpers/boot-time.helper");
 class MyDesklet extends Desklet.Desklet {
   constructor(metadata, deskletId) {
     super(metadata, deskletId);
+
+    const settings = new Settings.DeskletSettings(this, metadata["uuid"], deskletId);
+    settings.bindProperty(Settings.BindingDirection.IN, "loading-animation", "loadingAnimation", this._onSettingsChanged.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "show-accent-color", "showAccentColor", this._onSettingsChanged.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "accent-color", "accentColor", this._onSettingsChanged.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "hideDecorations", "hideDecorations", this.updateDecoration.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "label-color", "labelColor", this._onSettingsChanged.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "row-background-color", "rowBackgroundColor", this._onSettingsChanged.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "values-color", "valuesColor", this._onSettingsChanged.bind(this));
+    settings.bindProperty(Settings.BindingDirection.IN, "headline", "headline", this._onSettingsChanged.bind(this));
+
     this.setHeader(_("Boot Time"));
+    this.updateDecoration();
     this._setupUI();
   }
 
   async _setupUI() {
     let bootTimes;
-    if (this._loadingTimeoutId) Mainloop.source_remove(this._loadingTimeoutId);
+    if (this._loadingTimeoutId) {
+      Mainloop.source_remove(this._loadingTimeoutId);
+      this._loadingTimeoutId = null;
+    }
     try {
       bootTimes = await BootTimeHelper.getBootTime();
     } catch (e) {
       if (e.message === "Bootup is not yet finished") {
-        if (!this.loadingUIisLoaded) {
-          this.loadingUIisLoaded = true;
+        if (!this._loadingUIisLoaded) {
+          this._loadingUIisLoaded = true;
           this._getLoadingUI();
         }
         this._loadingTimeoutId = Mainloop.timeout_add_seconds(1, () => this._setupUI());
@@ -42,26 +58,32 @@ class MyDesklet extends Desklet.Desklet {
         return;
       }
     }
-    if (bootTimes) this._getBootTimeUI(bootTimes);
+    if (bootTimes) {
+      if (this._animationTimeoutId) {
+        Mainloop.source_remove(this._animationTimeoutId);
+        this._animationTimeoutId = null;
+      }
+      this._getBootTimeUI(bootTimes);
+    }
   }
 
   _getBootTimeUI(bootTimes) {
-    if (this._animationTimeoutId) {
-      Mainloop.source_remove(this._animationTimeoutId);
-      this._animationTimeoutId = null;
-    }
     const mainContainer = new St.BoxLayout({ vertical: true, style_class: "boot-time-main-container" });
-    const headline = new St.Label({ text: _("Boot Time"), style_class: "boot-time-headline" });
-    mainContainer.add_child(headline);
-
+    if (this.headline) {
+      const headline = new St.Label({ text: _("Boot Time"), style_class: "boot-time-headline" });
+      mainContainer.add_child(headline);
+    }
     let widestLabelSize = 0;
     const labels = [];
     for (const bootTime of bootTimes) {
       const row = new St.BoxLayout({ x_expand: true, style_class: "boot-time-row" });
+      row.set_style(`background-color:${this.rowBackgroundColor};`);
 
-      const label = new St.Label({ text: bootTime.label, style_class: "boot-time-label" });
-      if (bootTime.name === "Total") {
-        row.style_class = "boot-time-row-total";
+      const label = new St.Label({ text: bootTime.label });
+      label.set_style(`color: ${this.labelColor}; font-weight: bold;`);
+
+      if (bootTime.name === "Total" && this.showAccentColor) {
+        row.set_style(row.style + ` border: solid 1px ${this.accentColor};`);
       }
       labels.push(label);
       row.add_child(label);
@@ -71,17 +93,29 @@ class MyDesklet extends Desklet.Desklet {
       if (width > widestLabelSize) widestLabelSize = width;
 
       // Value Label
-      row.add_child(new St.Label({ text: bootTime.value }));
+      const value = new St.Label({ text: bootTime.value });
+      value.set_style(`color: ${this.valuesColor};`);
+      row.add_child(value);
 
       mainContainer.add_child(row);
     }
 
     widestLabelSize += widestLabelSize / 10;
     for (const label of labels) {
-      label.set_style(`width: ${widestLabelSize}px;`);
+      label.set_style(label.style + ` width: ${widestLabelSize}px;`);
     }
 
     this.setContent(mainContainer);
+  }
+
+  _onSettingsChanged() {
+    this._loadingUIisLoaded = false;
+    this._setupUI();
+  }
+
+  updateDecoration() {
+    this.metadata["prevent-decorations"] = this.hideDecorations;
+    this._updateDecoration();
   }
 
   _getLoadingUI() {
@@ -89,7 +123,7 @@ class MyDesklet extends Desklet.Desklet {
       layout_manager: new Clutter.BinLayout(),
       style_class: "boot-time-loading-container",
     });
-    loadingContainer.add_child(this._drawCircle());
+    if (this.loadingAnimation) loadingContainer.add_child(this._drawCircle());
     const loadingLabel = new Clutter.Text({
       text: _("Boot process not yet complete..."),
       line_wrap: true,
@@ -104,7 +138,7 @@ class MyDesklet extends Desklet.Desklet {
     });
     loadingContainer.add_child(bin);
     this.setContent(loadingContainer);
-    this._startAnimation();
+    if (this.loadingAnimation) this._startAnimation();
   }
 
   _showErrorUI(errorText) {
@@ -127,16 +161,23 @@ class MyDesklet extends Desklet.Desklet {
     errorContainer.add_child(icon);
     errorContainer.add_child(bin);
     this.setContent(errorContainer);
-    if (this._timeout) Mainloop.source_remove(this._timeout);
   }
 
   _startAnimation() {
     this.animationState = 0;
-    if (this._animationTimeoutId) Mainloop.source_remove(this._animationTimeoutId);
+    if (this._animationTimeoutId) {
+      Mainloop.source_remove(this._animationTimeoutId);
+      this._animationTimeoutId = null;
+    }
     this._animationTimeoutId = Mainloop.timeout_add(20, this._updateAnimation.bind(this));
   }
 
   _updateAnimation() {
+    if (!this.loadingAnimation) {
+      Mainloop.source_remove(this._animationTimeoutId);
+      this._animationTimeoutId = null;
+      return false;
+    }
     this.animationState += 0.01;
     if (this.animationState > 1) this.animationState = 0;
 
