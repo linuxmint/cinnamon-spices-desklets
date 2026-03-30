fb14d31e91604cd81c00c1ef53941e0281830797
Rico <mailvonrico@gmail.com>
2026-03-30 23:03:12 +0200

    input validation, am pm toggle

--- alarm-clock@KopfdesDaemons/files/alarm-clock@KopfdesDaemons/desklet.js
+++ alarm-clock@KopfdesDaemons/files/alarm-clock@KopfdesDaemons/desklet.js
@@ -2,6 +2,7 @@ const Desklet = imports.ui.desklet;
 const St = imports.gi.St;
 const Cinnamon = imports.gi.Cinnamon;
 const Settings = imports.ui.settings;
+const Gio = imports.gi.Gio;
 
 class MyDesklet extends Desklet.Desklet {
   constructor(metadata, deskletId) {
@@ -9,24 +10,28 @@ class MyDesklet extends Desklet.Desklet {
     this.setHeader("Alarm Clock");
 
     this._isReloading = false;
+    this._desktop_settings = new Gio.Settings({ schema_id: "org.cinnamon.desktop.interface" });
+    this._clockUse24hId = this._desktop_settings.connect("changed::clock-use-24h", () => this._setupLayout());
 
     // Default settings
     this.scaleSize = 1;
     this.alarmName = "Alarm";
     this.alarmDays = [];
     this.alarmIsEnabled = false;
+    this.amPm = "am";
 
     // Bind settings
     this.settings = new Settings.DeskletSettings(this, metadata["uuid"], deskletId);
     this.settings.bindProperty(Settings.BindingDirection.IN, "alarm-days", "alarmDays", this._setupLayout);
     this.settings.bindProperty(Settings.BindingDirection.IN, "alarm-is-enabled", "alarmIsEnabled", this._setupLayout);
+    this.settings.bindProperty(Settings.BindingDirection.IN, "am-pm", "amPm", this._setupLayout);
   }
 
   on_desklet_added_to_desktop() {
     this._setupLayout();
   }
 
-  on_desklet_removed_from_desktop() {
+  on_desklet_removed() {
     if (this.settings && !this._isReloading) {
       this.settings.finalize();
     }
@@ -69,9 +74,11 @@ class MyDesklet extends Desklet.Desklet {
 
     mainContainer.add(dayButtonsRow);
 
+    const inputsRow = new St.BoxLayout({ vertical: false, y_align: St.Align.MIDDLE });
+
     const getInput = () => {
       return new St.Entry({
-        text: "00",
+        hint_text: "00",
         style: `font-size: ${this.scaleSize * 2}em; padding: ${this.scaleSize * 0.3}em; border-radius: ${this.scaleSize * 0.5}em;`,
         reactive: true,
         track_hover: true,
@@ -80,13 +87,47 @@ class MyDesklet extends Desklet.Desklet {
     };
 
     this.inputHours = getInput();
+    inputsRow.add(this.inputHours);
+
+    const separator = new St.Bin({ child: new St.Label({ text: ":" }), style: `font-size: ${this.scaleSize * 2}em; padding: 0 ${this.scaleSize * 0.05}em;` });
+    inputsRow.add(separator);
+
     this.inputMinutes = getInput();
+    inputsRow.add(this.inputMinutes);
 
     // Function for input events
     const grabFocus = input => {
       global.set_stage_input_mode(Cinnamon.StageInputMode.FOCUSED);
       input.grab_key_focus();
       global.set_stage_input_mode(Cinnamon.StageInputMode.NORMAL);
+    };
+
+    const validateInput = (input, minutesOrHours) => {
+      const use24h = this._desktop_settings.get_boolean("clock-use-24h");
+      const originalText = input.get_text();
+
+      let filteredText = originalText.replace(/[^0-9]/g, "");
+
+      if (filteredText.length > 2) {
+        filteredText = filteredText.slice(0, 2);
+      }
+
+      if (filteredText) {
+        let num = parseInt(filteredText, 10);
+
+        if (minutesOrHours === "hours") {
+          const maxHours = use24h ? 23 : 12;
+          if (num > maxHours) {
+            filteredText = maxHours.toString();
+          }
+        } else if (minutesOrHours === "minutes") {
+          if (num > 59) {
+            filteredText = "59";
+          }
+        }
+      }
+
+      if (originalText !== filteredText) input.set_text(filteredText);
     };
 
     const submit = () => {
@@ -97,10 +138,26 @@ class MyDesklet extends Desklet.Desklet {
     // Connect input events
     this.inputHours.clutter_text.connect("button-press-event", () => grabFocus(this.inputHours));
     this.inputMinutes.clutter_text.connect("button-press-event", () => grabFocus(this.inputMinutes));
+    this.inputHours.clutter_text.connect("text-changed", () => validateInput(this.inputHours, "hours"));
+    this.inputMinutes.clutter_text.connect("text-changed", () => validateInput(this.inputMinutes, "minutes"));
     this.inputMinutes.clutter_text.connect("activate", () => submit());
     this.inputHours.clutter_text.connect("activate", () => submit());
 
-    const separator = new St.Bin({ child: new St.Label({ text: ":" }), style: `font-size: ${this.scaleSize * 2}em; padding: 0 ${this.scaleSize * 0.05}em;` });
+    if (!this._desktop_settings.get_boolean("clock-use-24h")) {
+      const amPmButton = new St.Button({
+        child: new St.Label({ text: this.amPm.toUpperCase(), style: `font-size: ${this.scaleSize * 1.5}em;` }),
+        style: `padding: ${this.scaleSize * 0.2}em; margin: 0 ${this.scaleSize * 0.2}em; border-radius: ${this.scaleSize * 0.5}em;`,
+        style_class: "alarm-clock-am-pm-button",
+      });
+
+      amPmButton.connect("clicked", () => {
+        this.amPm = this.amPm === "am" ? "pm" : "am";
+        amPmButton.get_child().set_text(this.amPm.toUpperCase());
+      });
+
+      const box = new St.Bin({ child: amPmButton, y_align: St.Align.START });
+      inputsRow.add(box);
+    }
 
     // Inner circle of the toggle button
     const baseToggleCircleStyle = `border-radius: ${this.scaleSize * 0.8}em; width: ${this.scaleSize * 1.5}em; height: ${this.scaleSize * 1.5}em;`;
@@ -126,15 +183,8 @@ class MyDesklet extends Desklet.Desklet {
       toggleButton.set_style(this.alarmIsEnabled ? aktiveToggleStyle : inaktiveToggleStyle);
     });
 
-    // Container for the toggle button for alignment
-    const toggleBin = new St.Bin({ child: toggleButton });
-
-    // Add elements to the inputs row
-    const inputsRow = new St.BoxLayout({ vertical: false, y_align: St.Align.MIDDLE });
-    inputsRow.add(this.inputHours);
-    inputsRow.add(separator);
-    inputsRow.add(this.inputMinutes);
     inputsRow.add(new St.Bin({ x_expand: true }));
+    const toggleBin = new St.Bin({ child: toggleButton });
     inputsRow.add(toggleBin);
 
     mainContainer.add(inputsRow);
