1c79b31ffbf6b90a8a1c7a491725d2d67b3d127d
Rico <mailvonrico@gmail.com>
2026-04-09 17:07:30 +0200

    generate colors + replace function

--- adventCandles@KopfdesDaemons/files/adventCandles@KopfdesDaemons/desklet.js
+++ adventCandles@KopfdesDaemons/files/adventCandles@KopfdesDaemons/desklet.js
@@ -7,6 +7,8 @@ const Gettext = imports.gettext;
 const Clutter = imports.gi.Clutter;
 const GdkPixbuf = imports.gi.GdkPixbuf;
 const Cogl = imports.gi.Cogl;
+const Gio = imports.gi.Gio;
+const ByteArray = imports.byteArray;
 
 const UUID = "adventCandles@KopfdesDaemons";
 
@@ -29,11 +31,19 @@ class MyDesklet extends Desklet.Desklet {
     // Default settings values
     this.deskletScale = 1;
     this.animationSpeed = 300;
+    this.candle1Color = "red";
+    this.candle2Color = "green";
+    this.candle3Color = "blue";
+    this.candle4Color = "yellow";
 
     // Bind settings properties
     this.settings = new Settings.DeskletSettings(this, metadata["uuid"], deskletId);
     this.settings.bindProperty(Settings.BindingDirection.IN, "desklet-scale", "deskletScale", this._onSettingsChanged);
     this.settings.bindProperty(Settings.BindingDirection.IN, "animation-speed", "animationSpeed", this._onSettingsChanged);
+    this.settings.bindProperty(Settings.BindingDirection.IN, "candle-1-color", "candle1Color", this._onSettingsChanged);
+    this.settings.bindProperty(Settings.BindingDirection.IN, "candle-2-color", "candle2Color", this._onSettingsChanged);
+    this.settings.bindProperty(Settings.BindingDirection.IN, "candle-3-color", "candle3Color", this._onSettingsChanged);
+    this.settings.bindProperty(Settings.BindingDirection.IN, "candle-4-color", "candle4Color", this._onSettingsChanged);
   }
 
   on_desklet_added_to_desktop() {
@@ -173,8 +183,90 @@ class MyDesklet extends Desklet.Desklet {
     }
   }
 
+  _generateColorVariants(colorString) {
+    // Nutze Clutter, um den Farb-String (egal ob Hex, rgb() oder Farbname) in RGB-Werte zu zerlegen
+    let [res, color] = Clutter.Color.from_string(colorString);
+    if (!res) {
+      // Falls das Parsen fehlschlägt, gib den Text unverändert zurück
+      return { a: colorString, b: colorString, c: colorString };
+    }
+
+    const toHex = (r, g, b) => {
+      return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+    };
+
+    // Variante A: Basis-Farbe
+    let a = toHex(color.red, color.green, color.blue);
+
+    // Variante B: Hellere Farbe (mischt ca. 30% Weiß dazu)
+    let b = toHex(
+      Math.min(255, Math.round(color.red + (255 - color.red) * 0.3)),
+      Math.min(255, Math.round(color.green + (255 - color.green) * 0.3)),
+      Math.min(255, Math.round(color.blue + (255 - color.blue) * 0.3)),
+    );
+
+    // Variante C: Dunklere Farbe (mischt ca. 30% Schwarz dazu)
+    let c = toHex(Math.max(0, Math.round(color.red * 0.7)), Math.max(0, Math.round(color.green * 0.7)), Math.max(0, Math.round(color.blue * 0.7)));
+
+    return { a, b, c };
+  }
+
   _getImageAtScale(imageFileName, width, height) {
-    const pixBuf = GdkPixbuf.Pixbuf.new_from_file_at_size(imageFileName, width, height);
+    let pixBuf = null;
+
+    if (imageFileName.endsWith(".svg")) {
+      try {
+        if (!this._svgCache) this._svgCache = {};
+
+        let svgString;
+        if (this._svgCache[imageFileName]) {
+          svgString = this._svgCache[imageFileName];
+        } else {
+          const file = Gio.File.new_for_path(imageFileName);
+          const [success, contents] = file.load_contents(null);
+          if (success) {
+            svgString = ByteArray.toString(contents);
+            this._svgCache[imageFileName] = svgString;
+          }
+        }
+
+        if (svgString) {
+          const c1 = this._generateColorVariants(this.candle1Color);
+          const c2 = this._generateColorVariants(this.candle2Color);
+          const c3 = this._generateColorVariants(this.candle3Color);
+          const c4 = this._generateColorVariants(this.candle4Color);
+
+          let coloredSvgString = svgString
+            .replace(/%CANDLE_1_COLOR_A%/g, c1.a)
+            .replace(/%CANDLE_1_COLOR_B%/g, c1.b)
+            .replace(/%CANDLE_1_COLOR_C%/g, c1.c)
+            .replace(/%CANDLE_2_COLOR_A%/g, c2.a)
+            .replace(/%CANDLE_2_COLOR_B%/g, c2.b)
+            .replace(/%CANDLE_2_COLOR_C%/g, c2.c)
+            .replace(/%CANDLE_3_COLOR_A%/g, c3.a)
+            .replace(/%CANDLE_3_COLOR_B%/g, c3.b)
+            .replace(/%CANDLE_3_COLOR_C%/g, c3.c)
+            .replace(/%CANDLE_4_COLOR_A%/g, c4.a)
+            .replace(/%CANDLE_4_COLOR_B%/g, c4.b)
+            .replace(/%CANDLE_4_COLOR_C%/g, c4.c)
+            .replace(/%CANDLE_1_COLOR%/g, c1.a)
+            .replace(/%CANDLE_2_COLOR%/g, c2.a)
+            .replace(/%CANDLE_3_COLOR%/g, c3.a)
+            .replace(/%CANDLE_4_COLOR%/g, c4.a);
+
+          const bytes = ByteArray.fromString(coloredSvgString);
+          const stream = Gio.MemoryInputStream.new_from_bytes(GLib.Bytes.new(bytes));
+          pixBuf = GdkPixbuf.Pixbuf.new_from_stream_at_scale(stream, width, height, true, null);
+        }
+      } catch (e) {
+        global.logError("Error dynamically coloring SVG: " + e);
+      }
+    }
+
+    if (!pixBuf) {
+      pixBuf = GdkPixbuf.Pixbuf.new_from_file_at_size(imageFileName, width, height);
+    }
+
     const image = new Clutter.Image();
     image.set_data(pixBuf.get_pixels(), pixBuf.get_has_alpha() ? Cogl.PixelFormat.RGBA_8888 : Cogl.PixelFormat.RGBA_888, width, height, pixBuf.get_rowstride());
 
