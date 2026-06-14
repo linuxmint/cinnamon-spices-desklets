a8b475d33e5c1e1d2373f3cd6578337a288db688
Milchreis <nick-m@gmx.de>
2026-06-14 11:10:24 +0200

    fixes for the warnings

--- simple-battery@suckatcoding.com/files/simple-battery@suckatcoding.com/desklet.js
+++ simple-battery@suckatcoding.com/files/simple-battery@suckatcoding.com/desklet.js
@@ -2,11 +2,13 @@ const Desklet = imports.ui.desklet;
 const St = imports.gi.St;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const ByteArray = imports.byteArray;
 const Settings = imports.ui.settings;
 const Gettext = imports.gettext;
 
 const UUID = "simple-battery@suckatcoding.com";
-const DESKLET_DIR = GLib.get_home_dir() + "/.local/share/cinnamon/desklets/" + UUID;
+const DESKLET_DIR = GLib.get_user_data_dir() + "/cinnamon/desklets/" + UUID;
 Gettext.bindtextdomain(UUID, DESKLET_DIR + "/locale");
 
 function _(str) {
@@ -69,71 +71,111 @@ SimpleBatteryDesklet.prototype = {
 
     update: function() {
         try {
-            let cmdBat = 'bash -c "upower -i $(upower -e | grep -i bat | head -n 1)"';
-            let [resBat, outBat] = GLib.spawn_command_line_sync(cmdBat);
+            let procBat = Gio.Subprocess.new(
+                ['bash', '-c', 'upower -i $(upower -e | grep -i bat | head -n 1)'],
+                Gio.SubprocessFlags.STDOUT_PIPE
+            );
             
-            if (resBat) {
-                let output = outBat.toString();
-                let matchPct = output.match(/percentage:\s+(\d+)%/);
-                let matchEmpty = output.match(/time to empty:\s+(.*)/);
-                let matchFull = output.match(/time to full:\s+(.*)/);
-                let matchState = output.match(/state:\s+(.*)/);
-                
-                let pctNum = matchPct ? parseInt(matchPct[1]) : 0;
-                
-                this.pctLabel.set_text(matchPct ? matchPct[1] + "%" : "--%");
-                let fillWidth = Math.round(150 * (pctNum / 100));
-                this.fill.set_style("width: " + fillWidth + "px;");
-                
-                let state = matchState ? matchState[1] : "";
-                if (state === "discharging" && matchEmpty) {
-                    this.timeLabel.set_text(matchEmpty[1] + " " + _("remaining"));
-                } else if (state === "charging" && matchFull) {
-                    this.timeLabel.set_text(matchFull[1] + " " + _("until full"));
-                } else if (state === "fully-charged") {
-                    this.timeLabel.set_text(_("Fully charged"));
-                } else {
-                    this.timeLabel.set_text(_("AC Power"));
-                }
-
-                let cmdUsage = `bash -c "if [ ! -f /tmp/desklet_start_bat.txt ]; then echo ${pctNum} > /tmp/desklet_start_bat.txt; fi; cat /tmp/desklet_start_bat.txt"`;
-                let [resUsage, outUsage] = GLib.spawn_command_line_sync(cmdUsage);
-                
-                if (resUsage) {
-                    let startPct = parseInt(outUsage.toString().trim());
-                    let diff = startPct - pctNum;
-                    
-                    if (diff > 0) {
-                        this.usageLabel.set_text(_("Usage:") + " " + diff + "% " + _("since start"));
-                    } else if (diff < 0) {
-                        this.usageLabel.set_text(_("Charged:") + " " + Math.abs(diff) + "% " + _("since start"));
-                    } else {
-                        this.usageLabel.set_text(_("Usage:") + " 0% " + _("since start"));
+            procBat.communicate_utf8_async(null, null, (proc, res) => {
+                try {
+                    let [ok, outBat, stderr] = proc.communicate_utf8_finish(res);
+                    if (ok && outBat) {
+                        this.parseAndUpdate(outBat);
                     }
+                } catch (e) {
+                    this.timeLabel.set_text(_("Error reading data"));
                 }
-            }
-
-            let [resUptime, outUptime] = GLib.spawn_command_line_sync("cat /proc/uptime");
-            if (resUptime) {
-                let uptimeSeconds = parseFloat(outUptime.toString().split(" ")[0]);
-                let hours = Math.floor(uptimeSeconds / 3600);
-                let minutes = Math.floor((uptimeSeconds % 3600) / 60);
-                
-                if (hours > 0) {
-                    this.uptimeLabel.set_text(_("Uptime:") + " " + hours + _("h") + " " + minutes + _("min"));
-                } else {
-                    this.uptimeLabel.set_text(_("Uptime:") + " " + minutes + _("min"));
-                }
-            }
+            });
 
         } catch (e) {
             this.timeLabel.set_text(_("Error reading data"));
         }
 
+        if (this.timeout) Mainloop.source_remove(this.timeout);
         this.timeout = Mainloop.timeout_add_seconds(60, () => {
             this.update();
             return false; 
         });
+    },
+
+    parseAndUpdate: function(output) {
+        let matchPct = output.match(/percentage:\s+(\d+)%/);
+        let matchEmpty = output.match(/time to empty:\s+(.*)/);
+        let matchFull = output.match(/time to full:\s+(.*)/);
+        let matchState = output.match(/state:\s+(.*)/);
+        
+        let pctNum = matchPct ? parseInt(matchPct[1]) : 0;
+        
+        this.pctLabel.set_text(matchPct ? matchPct[1] + "%" : "--%");
+        let fillWidth = Math.round(150 * (pctNum / 100));
+        this.fill.set_style("width: " + fillWidth + "px;");
+        
+        let state = matchState ? matchState[1] : "";
+        if (state === "discharging" && matchEmpty) {
+            this.timeLabel.set_text(matchEmpty[1] + " " + _("remaining"));
+        } else if (state === "charging" && matchFull) {
+            this.timeLabel.set_text(matchFull[1] + " " + _("until full"));
+        } else if (state === "fully-charged") {
+            this.timeLabel.set_text(_("Fully charged"));
+        } else {
+            this.timeLabel.set_text(_("AC Power"));
+        }
+
+        let tmpPath = "/tmp/desklet_start_bat_" + UUID + ".txt";
+        let tmpFile = Gio.File.new_for_path(tmpPath);
+
+        tmpFile.load_contents_async(null, (file, res) => {
+            try {
+                let [success, contents] = file.load_contents_finish(res);
+                if (success) {
+                    let contentStr = ByteArray.toString(contents).trim();
+                    let startPct = contentStr !== "" ? parseInt(contentStr) : pctNum;
+                    this.updateUsageUI(startPct, pctNum);
+                }
+            } catch (e) {
+                file.replace_contents_async(
+                    pctNum.toString(),
+                    null,
+                    false,
+                    Gio.FileCreateFlags.NONE,
+                    null,
+                    (f, r) => {
+                        try { f.replace_contents_finish(r); } catch (err) {}
+                    }
+                );
+                this.updateUsageUI(pctNum, pctNum);
+            }
+        });
+
+        let uptimeFile = Gio.File.new_for_path("/proc/uptime");
+        uptimeFile.load_contents_async(null, (file, res) => {
+            try {
+                let [success, contents] = file.load_contents_finish(res);
+                if (success) {
+                    let outUptime = ByteArray.toString(contents);
+                    let uptimeSeconds = parseFloat(outUptime.split(" ")[0]);
+                    let hours = Math.floor(uptimeSeconds / 3600);
+                    let minutes = Math.floor((uptimeSeconds % 3600) / 60);
+                    
+                    if (hours > 0) {
+                        this.uptimeLabel.set_text(_("Uptime:") + " " + hours + _("h") + " " + minutes + _("min"));
+                    } else {
+                        this.uptimeLabel.set_text(_("Uptime:") + " " + minutes + _("min"));
+                    }
+                }
+            } catch (e) {}
+        });
+    },
+
+    updateUsageUI: function(startPct, currentPct) {
+        let diff = startPct - currentPct;
+        if (diff > 0) {
+            this.usageLabel.set_text(_("Usage:") + " " + diff + "% " + _("since start"));
+        } else if (diff < 0) {
+            this.usageLabel.set_text(_("Charged:") + " " + Math.abs(diff) + "% " + _("since start"));
+        } else {
+            this.usageLabel.set_text(_("Usage:") + " 0% " + _("since start"));
+        }
     },
 
     on_desklet_removed: function() {
